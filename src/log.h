db604551b56696d9809642b4668c35a74d354b29
Thomas Nilefalk <thomas@nilefalk.se>
2017-12-08 21:24:51 +0100

    Add a few TODOs to log.[ch]

--- src/log.h
+++ src/log.h
@@ -17,6 +17,7 @@ typedef void (*log_LockFn)(void *udata, int lock);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
+/* TODO: should set up for conditionally making some macros NoOPs using #ifdef's */
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define log_info(...)  log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
