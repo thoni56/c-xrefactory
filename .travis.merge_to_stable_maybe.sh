b3e39dce8269e9b2b7e51a91ca27f152b514048c
Thomas Nilefalk <thomas@nilefalk.se>
2018-01-10 19:23:09 +0100

    Merge master to stable if successful Travis build

---
+++ .travis.merge_to_stable_maybe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "We're not building on the master branch. Not merging to stable."
+  exit 0
+fi
+
+echo "Merging to stable..."
+
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+git branch -v -f stable master
+git checkout stable
+git push https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG
