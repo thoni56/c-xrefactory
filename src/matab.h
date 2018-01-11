8a8d7a267854c0bcd76d56efe24ac7f30efa0ebe
Thomas Nilefalk <thomas@nilefalk.se>
2018-01-11 11:33:03 +0100

    Break out macro argument table from yylex into separate module

---
+++ src/matab.h
@@ -0,0 +1,19 @@
+#ifndef _MATAB_H_
+#define _MATAB_H_
+
+/* Macro argument table - instance of hashtable */
+
+#include "proto.h"
+
+#define HASH_TAB_TYPE struct maTab
+#define HASH_ELEM_TYPE S_macroArgTabElem
+#define HASH_FUN_PREFIX maTab
+
+#include "hashtab.th"
+
+extern struct maTab s_maTab;
+
+#undef HASH_TAB_TYPE
+#undef HASH_ELEM_TYPE
+#undef HASH_FUN_PREFIX
+#endif
