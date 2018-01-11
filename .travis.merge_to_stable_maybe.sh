b273afb4622ce6a5bb5bca24c0d38813a24f9751
Thomas Nilefalk <thomas@nilefalk.se>
2018-01-11 01:08:24 +0100

    Pull before push updating stable branch after successful Travis build

--- .travis.merge_to_stable_maybe.sh
+++ .travis.merge_to_stable_maybe.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "We're not building on the master branch. Not merging to stable."
   exit 0
@@ -11,4 +13,6 @@ git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git branch -v -f stable master
 git checkout stable
+git status
+echo git push https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG
 git push https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG
