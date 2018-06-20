356560c52a5b7ae02736f6ffff6a460310a8ff23
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2018-06-20 16:20:28 +0200

    Minor edits

--- src/proto.h
+++ src/proto.h
@@ -4,7 +4,8 @@
 /*
 
   Structures herein will be subject to the filler, structure typedef
-  and enum strings generation done in the Makefile.common.
+  and enum strings generation done in the Makefile.common. See
+  description in NOTES.md.
 
   In the beginning of c-xrefs modern life this was a huge pile of
   anything, I'm trying to clean this up so that only things that
@@ -256,22 +257,22 @@ enum {
 
 enum taskRegimes {
     RegimeUndefined,
-    RegimeXref,			/* cross referencer called by user from command line */
-    RegimeEditServer,	/* editor server, called by on-line editing action */
+    RegimeXref, /* cross referencer called by user from command line */
+    RegimeEditServer, /* editor server, called by on-line editing action */
     RegimeGenerate,		/* generate str-fill macros, ... */
     RegimeHtmlGenerate,	/* generate html form of input files, ... */
-    RegimeRefactory,	/* generate html form of input files, ... */
+    RegimeRefactory,	/* refactoring server, called by on-line editing */
 };
 
-/* ************** on-line (browsing) commands for xrefsrv ********** */
+/* ************** on-line (browsing) operations for c-xref server  ********** */
 
 enum olcxOptions {
-    OLO_COMPLETION,		/* must be zero */
+    OLO_COMPLETION,             /* must be zero */
     OLO_SEARCH,
     OLO_TAG_SEARCH,
-    OLO_RENAME,			/* same as push, just another ordering */
-    OLO_ENCAPSULATE,	/* same as rename, remove private references */
-    OLO_ARG_MANIP,			/* as rename, constructors resolved as functions */
+    OLO_RENAME,        /* same as push, just another ordering */
+    OLO_ENCAPSULATE,   /* same as rename, remove private references */
+    OLO_ARG_MANIP, /* as rename, constructors resolved as functions */
     OLO_VIRTUAL2STATIC_PUSH, /* same as rename, another message on virtuals */
     OLO_GET_AVAILABLE_REFACTORINGS,
     OLO_PUSH,
@@ -293,17 +294,17 @@ enum olcxOptions {
     OLO_PUSH_ALL_IN_METHOD,
     OLO_PUSH_FOR_LOCALM,
     OLO_GOTO,
-    OLO_CGOTO,			/* goto completion item definition */
-    OLO_TAGGOTO,		/* goto tag search result */
-    OLO_TAGSELECT,		/* select tag search result */
-    OLO_CBROWSE,		/* browse javadoc of completion item */
+    OLO_CGOTO,                 /* goto completion item definition */
+    OLO_TAGGOTO,               /* goto tag search result */
+    OLO_TAGSELECT,             /* select tag search result */
+    OLO_CBROWSE,               /* browse javadoc of completion item */
     OLO_REF_FILTER_SET,
     OLO_REF_FILTER_PLUS,
     OLO_REF_FILTER_MINUS,
-    OLO_CSELECT,		/* select completion */
+    OLO_CSELECT,                /* select completion */
     OLO_COMPLETION_BACK,
     OLO_COMPLETION_FORWARD,
-    OLO_EXTRACT,		/* extract block into separate function */
+    OLO_EXTRACT,            /* extract block into separate function */
     OLO_CT_INSPECT_DEF,		/* inspect definition from class tree */
     OLO_MENU_INSPECT_DEF,	/* inspect definition from symbol menu */
     OLO_MENU_INSPECT_CLASS,	/* inspect class from symbol menu */
@@ -314,8 +315,8 @@ enum olcxOptions {
     OLO_MENU_FILTER_SET,	/* more strong filtering */
     OLO_MENU_FILTER_PLUS,	/* more strong filtering */
     OLO_MENU_FILTER_MINUS,	/* smaller filtering */
-    OLO_MENU_GO,		/* push references from selected menu items */
-    OLO_CHECK_VERSION,	/* check version correspondance */
+    OLO_MENU_GO,            /* push references from selected menu items */
+    OLO_CHECK_VERSION,      /* check version correspondance */
     OLO_RESET_REF_SUFFIX,	/* set n-th argument after argument insert */
     OLO_TRIVIAL_PRECHECK,	/* trivial pre-refactoring checks */
     OLO_MM_PRE_CHECK,		/* move method pre check */
@@ -323,25 +324,25 @@ enum olcxOptions {
     OLO_SAFETY_CHECK_INIT,
     OLO_SAFETY_CHECK1,
     OLO_SAFETY_CHECK2,
-    OLO_INTERSECTION,	/* just provide intersection of top references */
-    OLO_REMOVE_WIN,		/* just remove window of top references */
-    OLO_GOTO_DEF,		/* goto definition reference */
-    OLO_GOTO_CALLER,	/* goto caller reference */
-    OLO_SHOW_TOP,		/* show top symbol */
-    OLO_SHOW_TOP_APPL_CLASS,		/* show current reference appl class */
-    OLO_SHOW_TOP_TYPE,		/* show current symbol type */
-    OLO_SHOW_CLASS_TREE,		/* show current class tree */
-    OLO_TOP_SYMBOL_RES,	/* show top symbols resolution */
-    OLO_ACTIVE_PROJECT,		/* show active project name */
-    OLO_JAVA_HOME,		/* show inferred jdkclasspath */
-    OLO_REPUSH,			/* re-push pop-ed top */
-    OLO_CLASS_TREE,     /* display class tree */
-    OLO_USELESS_LONG_NAME,   /* display useless long class names */
-    OLO_USELESS_LONG_NAME_IN_CLASS,   /* display useless long class names */
-    OLO_MAYBE_THIS,   /* display 'this' class dependencies */
-    OLO_NOT_FQT_REFS,   /* display not fully qualified names in method */
-    OLO_NOT_FQT_REFS_IN_CLASS,   /* display not fully qualified names of class */
-    OLO_GET_ENV_VALUE,	/* get a value set by -set */
+    OLO_INTERSECTION,       /* just provide intersection of top references */
+    OLO_REMOVE_WIN,         /* just remove window of top references */
+    OLO_GOTO_DEF,           /* goto definition reference */
+    OLO_GOTO_CALLER,        /* goto caller reference */
+    OLO_SHOW_TOP,           /* show top symbol */
+    OLO_SHOW_TOP_APPL_CLASS,   /* show current reference appl class */
+    OLO_SHOW_TOP_TYPE,         /* show current symbol type */
+    OLO_SHOW_CLASS_TREE,       /* show current class tree */
+    OLO_TOP_SYMBOL_RES,        /* show top symbols resolution */
+    OLO_ACTIVE_PROJECT,        /* show active project name */
+    OLO_JAVA_HOME,             /* show inferred jdkclasspath */
+    OLO_REPUSH,                /* re-push pop-ed top */
+    OLO_CLASS_TREE,            /* display class tree */
+    OLO_USELESS_LONG_NAME,     /* display useless long class names */
+    OLO_USELESS_LONG_NAME_IN_CLASS, /* display useless long class names */
+    OLO_MAYBE_THIS,         /* display 'this' class dependencies */
+    OLO_NOT_FQT_REFS,       /* display not fully qualified names in method */
+    OLO_NOT_FQT_REFS_IN_CLASS, /* display not fully qualified names of class */
+    OLO_GET_ENV_VALUE,      /* get a value set by -set */
     OLO_SET_MOVE_TARGET,	/* set target place for moving action */
     OLO_SET_MOVE_CLASS_TARGET,	/* set target place for xref2 move class */
     OLO_SET_MOVE_METHOD_TARGET,	/* set target place for xref2 move method */
@@ -359,6 +360,7 @@ enum olcxOptions {
     OLO_GET_PARAM_COORDINATES,
     OLO_ABOUT,
 };
+
 
 /* ************************** refactorings **************************** */
 
@@ -604,10 +606,10 @@ enum types {
     TypeFunSep,			/* dummy, function separator for HTML */
     TypeSpecialComplet,	/* dummy special completion string (for(;xx!=NULL ..)*/
     TypeNonImportedClass,/* dummy for completion*/
-    TypeInducedError,         /* dummy in general*/
-    TypeInheritedFullMethod,   /* dummy for completion, complete whole definition */
+    TypeInducedError,    /* dummy in general*/
+    TypeInheritedFullMethod, /* dummy for completion, complete whole definition */
     TypeSpecialConstructorCompletion, /*dummy completion of constructor 'super'*/
-    TypeUnknown,   /* dummy for completion */
+    TypeUnknown,                      /* dummy for completion */
     MAX_TYPE,
     /* if this becomes greater than 256, increase SYMTYPES_LN !!!!!!!!!!!!! */
 };
