fa28d6f2445180543bad8c42addb08518f4294e6
Thomas Nilefalk <thomas@nilefalk.se>
2019-06-23 18:55:50 +0200

    Evolve unittest to handle two positions

--- src/tests/filetab_tests.c
+++ src/tests/filetab_tests.c
@@ -10,11 +10,13 @@ static struct fileTab fileTab;
 
 Ensure(FileTab, can_fetch_a_stored_filename) {
     S_fileItem fileItem = {"file.c"};
-    int position = 1;
+    int position_1 = 1;
+    int position_3 = 3;
     int fetched_position;
 
     fileTabInit(&fileTab, 5);
-    fileTabAdd(&fileTab, &fileItem, &position);
+    fileTabAdd(&fileTab, &fileItem, &position_1);
+    fileTabAdd(&fileTab, &fileItem, &position_3);
     assert_that(fileTabIsMember(&fileTab, &fileItem, &fetched_position));
-    assert_that(fetched_position, is_equal_to(position));
+    assert_that(fetched_position, is_equal_to(position_1));
 }
