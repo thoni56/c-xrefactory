f9a458a981663554c23157661b773a12a63d2d3a
Thomas Nilefalk <thomas@nilefalk.se>
2019-09-28 15:29:07 +0200

    Add a jslsemact unittest stub (no tests yet)

---
+++ src/unittests/jslsemact_tests.c
@@ -0,0 +1,37 @@
+#include <cgreen/cgreen.h>
+
+#include "classFileReader.mock"
+
+/* From globals.c */
+#include "filetab.h"
+
+S_fileTab s_fileTab;
+S_idIdent s_javaAnonymousClassName = {"{Anonymous}", NULL, {-1,0,0}};
+S_idIdent s_javaConstructorName = {"<init>", NULL, {-1,0,0}};
+S_typeModifiers * s_preCrTypesTab[MAX_TYPE];
+S_position s_noPos = {-1, 0, 0};
+S_symbol s_errorSymbol;
+S_options s_opt;        // current options
+int s_olOriginalFileNumber = -1;
+int s_noneFileIndex = -1;
+int ppmMemoryi=0;
+char ppmMemory[SIZE_ppmMemory];
+char *s_javaLangObjectLinkName="java/lang/Object";
+S_symbol s_defaultVoidDefinition;
+
+/* Mocks: */
+#include "jsltypetab.mock"
+#include "commons.mock"
+#include "jsemact.mock"
+#include "semact.mock"
+#include "cxref.mock"
+#include "misc.mock"
+#include "cct.mock"
+
+
+Describe(JslSemAct);
+BeforeEach(JslSemAct) {}
+AfterEach(JslSemAct) {}
+
+Ensure(JslSemAct, something) {
+}
