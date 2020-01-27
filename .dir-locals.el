9da379f00809637c4fa5dedfdd73b8fb9e064164
Thomas Nilefalk <thomas@nilefalk.se>
2020-01-27 08:45:54 +0100

    Add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . (
+            (indent-tabs-mode . nil)
+            (c-basic-offset . 4)
+            )
+         )
+ )
