6221deeadcd5b6a275f473f42c17df4e82bb82e2
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2020-01-27 16:33:13 +0100

    Remove unused special hash function for filetab (which was not different)

--- src/filetab.c
+++ src/filetab.c
@@ -7,13 +7,4 @@
 #define HASH_FUN(elemp) hashFun(elemp->name)
 #define HASH_ELEM_EQUAL(e1,e2) (strcmp(e1->name,e2->name)==0)
 
-static unsigned fileTabHashFun(char *ss) {
-    register unsigned h = 0;
-    register char *s = ss;
-    register char c;
-    for(c= *s; c ; c= *++s) SYMTAB_HASH_FUN_INC(h, c);
-    SYMTAB_HASH_FUN_FINAL(h);
-    return(h);
-}
-
 #include "hashtab.tc"
