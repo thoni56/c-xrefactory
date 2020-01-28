5ccaa85c4a42d8b5ebfe535233c078fc9c303d5d
Thomas Nilefalk <thomas@nilefalk.se>
2020-01-28 08:08:26 +0100

    Changed names of pipes to indicate direction

--- tests/sandboxed_emacs/c-xref-spy.c
+++ tests/sandboxed_emacs/c-xref-spy.c
@@ -9,24 +9,27 @@
 FILE *logFile;
 
 int main(int argc, char **argv) {
-    logFile = fopen("/Users/thomas/Utveckling/c-xrefactory/tests/sandboxed_emacs/c-xref-spy-input.log", "w");
+    logFile = fopen("/tmp/c-xref-spy-input.log", "w");
     for (int a=0; a<argc; a++)
         fprintf(logFile, "%s ", argv[a]);
     fprintf(logFile, "\n");
+    fflush(logFile);
+
+    sleep(20);
 
     /* Create the real c-xref process */
-    int stdinPipes[2];
-    int stdoutPipes[2];
+    int toCxrefPipe[2];
+    int fromCxrefPipe[2];
 
-    if (pipe(stdinPipes) < 0) {
-        perror("pipe(stdinPipes)");
+    if (pipe(toCxrefPipe) < 0) {
+        perror("pipe(toCxrefPipe)");
         _exit(-1);
     }
 
-    if (pipe(stdoutPipes) < 0) {
-        close(stdinPipes[0]);
-        close(stdinPipes[1]);
-        perror("pipe(stdoutPipes)");
+    if (pipe(fromCxrefPipe) < 0) {
+        close(toCxrefPipe[PIPE_READ]);
+        close(toCxrefPipe[PIPE_WRITE]);
+        perror("pipe(fromCxrefPipe)");
         _exit(-1);
     }
 
@@ -34,12 +37,11 @@ int main(int argc, char **argv) {
     if (pid == 0) {
         /* In the child, so... */
         /* ... connect the stdin... */
-        if (dup2(stdinPipes[PIPE_READ], STDIN_FILENO) == -1) exit(errno);
-        if (dup2(stdoutPipes[PIPE_WRITE], STDOUT_FILENO) == -1) exit(errno);
-        close(stdinPipes[PIPE_READ]);
-        close(stdinPipes[PIPE_WRITE]);
-        close(stdoutPipes[PIPE_READ]);
-        close(stdoutPipes[PIPE_WRITE]);
+        if (dup2(toCxrefPipe[PIPE_READ], STDIN_FILENO) == -1) exit(errno);
+        /* ... and stdout... */
+        //if (dup2(fromCxrefPipe[PIPE_WRITE], STDOUT_FILENO) == -1) exit(errno);
+        //close(toCxrefPipe[PIPE_WRITE]);
+        //close(fromCxrefPipe[PIPE_WRITE]);
         /* ... and exec ... */
         execv("../../src/c-xref", argv);
         /* ... if we get here execv failed... */
@@ -48,20 +50,26 @@ int main(int argc, char **argv) {
     }
 
     /* Parent... */
-    close(stdinPipes[PIPE_READ]);
-    close(stdinPipes[PIPE_WRITE]);
+    //close(toCxrefPipe[PIPE_READ]);
+    //close(fromCxrefPipe[PIPE_WRITE]);
+
 
     /* Read and propagate input, read and propagate output */
     /* Assuming the communication is synchronous!!! */
     char buffer[10000];
-    FILE *child_stdin = fdopen(stdoutPipes[PIPE_READ], "w");
-    FILE *child_stdout = fdopen(stdinPipes[PIPE_WRITE], "w");
+    FILE *toCxref = fdopen(toCxrefPipe[PIPE_WRITE], "w");
+    //FILE *fromCxref = fdopen(fromCxrefPipe[PIPE_READ], "r");
+
     while (fgets(buffer, 10000, stdin) != NULL) {
         fprintf(logFile, "<-:%s", buffer);
-        fputs(buffer, child_stdin);
-        fgets(buffer, 10000, child_stdout);
-        fprintf(logFile, "->:%s", buffer);
-        printf("%2s", buffer);
+        fflush(logFile);
+        fputs(buffer, toCxref); fflush(toCxref);
+        if (fgets(buffer, 10000, fromCxref) == NULL) {
+            fprintf(logFile, "Got NULL!\n"); fflush(logFile);
+            exit(1);
+        }
+        fprintf(logFile, "->:%s", buffer); fflush(logFile);
+        fprintf(stdout, "%s", buffer); fflush(stdout);
     }
 
     fclose(logFile);
