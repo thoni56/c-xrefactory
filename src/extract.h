ecc346428c653374024f9b8719707405aab53fdd
Thomas Nilefalk <thomas@nilefalk.se>
2020-02-19 00:04:55 +0100

    Remove duplicated extern declarations

--- src/extract.h
+++ src/extract.h
@@ -7,9 +7,7 @@ extern S_symbol * addContinueBreakLabelSymbol(int labn, char *name);
 extern void actionsBeforeAfterExternalDefinition(void);
 extern void extractActionOnBlockMarker(void);
 extern void genInternalLabelReference(int counter, int usage);
-extern S_symbol * addContinueBreakLabelSymbol(int labn, char *name);
 extern void deleteContinueBreakLabelSymbol(char *name);
-extern void genInternalLabelReference(int counter, int usage);
 extern void genContinueBreakReference(char *name);
 extern void genSwitchCaseFork(int lastFlag);
 
