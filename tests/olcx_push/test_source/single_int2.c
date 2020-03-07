21d20e14ef58f730e3bdba026b18f0581e462dc7
Thomas Nilefalk <thomas@nilefalk.se>
2020-03-07 17:40:16 +0100

    Make single_int2 differ from single_int1

--- tests/olcx_push/test_source/single_int2.c
+++ tests/olcx_push/test_source/single_int2.c
@@ -1,5 +1,5 @@
-int single_int_on_line_1_col_4;
-int  single_int_on_line_2_col_5;
+extern int single_int_on_line_1_col_4;
+extern int  single_int_on_line_2_col_5;
 
-single_int_on_line_1_col_4 = 3;
- single_int_on_line_2_col_5 = 5;
+single_int_on_line_1_col_4 = 222;
+ single_int_on_line_2_col_5 = 222;
