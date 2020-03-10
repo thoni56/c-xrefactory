a67877bc500fb7268f494b1d6de98db97169f061
Thomas Nilefalk <thomas@nilefalk.se>
2020-03-10 23:16:35 +0100

    Forgot this include when reverting to .h for generated header files

--- src/stdinc.h
+++ src/stdinc.h
@@ -6,7 +6,7 @@
    since the whole point is that the compiler already automatically adds them.
  */
 #ifdef GENERATION
-#include "compiler_defines.g"
+#include "compiler_defines.g.h"
 #endif
 
 #if defined(__WIN32) && (! defined(__WIN32__))
