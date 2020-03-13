c869511a8a4421af05e75a5167a7c47289dbfda1
Thomas Nilefalk <thomas@nilefalk.se>
2020-03-13 08:18:01 +0100

    Use real hash instead of mock

--- src/unittests/filetab_tests.c
+++ src/unittests/filetab_tests.c
@@ -17,10 +17,6 @@ Ensure(FileTab, can_fetch_a_stored_filename) {
     int position_3 = 3;
     int fetched_position;
 
-    expect(hashFun, will_return(5));
-    expect(hashFun, will_return(6));
-    expect(hashFun, will_return(5)); /* Same as first hash */
-
     fileTabInit(&fileTab, 5);
     fileTabAdd(&fileTab, &fileItem, &position_1);
     fileTabAdd(&fileTab, &fileItem, &position_3);
@@ -33,10 +29,6 @@ Ensure(FileTab, cannot_find_filename_not_in_tab) {
     S_fileItem exists = {"exists.c"};
     S_fileItem donotexist = {"donot_exist.c"};
     int position_1 = -1;
-
-    expect(hashFun, will_return(5)); /* Hash for exists - add */
-    expect(hashFun, will_return(5)); /* again - isMember */
-    expect(hashFun, will_return(6)); /* Has for donotexist - isMember */
 
     fileTabInit(&fileTab, 5);
     fileTabAdd(&fileTab, &exists, &position_1);
