90b7e79eb68c61b18e0efb199bb015800db5dcf1
Thomas Nilefalk <thomas@nilefalk.se>
2020-04-13 15:45:43 +0200

    Add a test for options

--- src/unittests/jslsemact_tests.c
+++ src/unittests/jslsemact_tests.c
@@ -9,7 +9,6 @@ S_id s_javaConstructorName = {"<init>", NULL, {-1,0,0}};
 S_typeModifier * s_preCreatedTypesTable[MAX_TYPE];
 Symbol s_errorSymbol;
 S_options s_opt;        // current options
-int ppmMemoryi=0;
 char ppmMemory[SIZE_ppmMemory];
 char *s_javaLangObjectLinkName="java/lang/Object";
 Symbol s_defaultVoidDefinition;
