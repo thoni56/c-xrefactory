7318c786798297182364eab6d94f5aaa3759e416
Thomas Nilefalk <thomas@nilefalk.se>
2020-04-17 20:33:25 +0200

    Clean up the ZLIB options

--- src/sources.mk
+++ src/sources.mk
@@ -1,12 +1,12 @@
-SRCS = main.c globals.c misc.c semact.c commons.c generate.c \
-	   enumTxt.c complete.c cxref.c cxfile.c lex.c yylex.c options.c \
-	   caching.c jsemact.c classfilereader.c classcaster.c init.c \
-	   editor.c refactory.c protocol.c log.c \
-	   html.c extract.c classhierarchy.c jslsemact.c \
-	   filetab.c macroargumenttable.c olcxtab.c editorbuffertab.c symboltable.c javafqttab.c \
-	   jsltypetab.c reftab.c memory.c utils.c characterbuffer.c hash.c symbol.c \
-	   c_parser.tab.c cexp_parser.tab.c java_parser.tab.c yacc_parser.tab.c \
-	   fileitem.c filedescriptor.c typemodifier.c position.c id.c
+SRCS =  main.c globals.c misc.c semact.c commons.c generate.c \
+		enumTxt.c complete.c cxref.c cxfile.c lex.c yylex.c options.c \
+		caching.c jsemact.c classfilereader.c classcaster.c init.c \
+		editor.c refactory.c protocol.c log.c \
+		html.c extract.c classhierarchy.c jslsemact.c \
+		filetab.c macroargumenttable.c olcxtab.c editorbuffertab.c symboltable.c \
+		javafqttab.c jsltypetab.c reftab.c memory.c utils.c characterbuffer.c hash.c \
+		symbol.c c_parser.tab.c cexp_parser.tab.c java_parser.tab.c yacc_parser.tab.c \
+		fileitem.c filedescriptor.c typemodifier.c position.c id.c
 
 OBJDIR = .objects
 OBJS = $(addprefix $(OBJDIR)/,${SRCS:.c=.o})
@@ -25,14 +25,8 @@ $(OBJS) $(DEPS): $(OBJDIR)
 $(ZLIB_LIB):
 	make -C $(ROOTDIR)/lib/zlib libz.a
 
-# Put the following into comments if you must build without zlib
-# library, i.e. without possibility to read compressed .jar files
-ZLIB_OPT=-DUSE_LIBZ
-ZLIB_LIB= -lz
-
 # If you are using your systems zlib use these
-INCLUDES=$(ZLIB_INCLUDE)
-LIBS=$(ZLIB_LIB)
+LIBS+=-lz
 # Else build the local, included, version and use these
 # ZLIB_LIB=$(ROOTDIR)/lib/zlib/libz.a
 # ZLIB_INCLUDE=-I$(ROOTDIR)/lib/zlib
