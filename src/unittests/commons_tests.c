5ee1c77edb865b06e53ddf5e9f2c8b550ec65ac7
Thomas Nilefalk <thomas@nilefalk.se>
2020-05-03 08:51:01 +0200

    Clean up extractPath() unittest

--- src/unittests/commons_tests.c
+++ src/unittests/commons_tests.c
@@ -16,22 +16,20 @@ AfterEach(Commons) {}
 
 
 Ensure(Commons, will_extract_nothing_if_no_path) {
+    char *source = "nopath";
     char dest[100];
-    char source[100];
     int length;
 
-    strcpy(source, "nopath");
     length = extractPathInto(source, dest);
     assert_that(strlen(dest) == 0); /* No path to copy */
     assert_that(length, is_equal_to(0));
 }
 
 Ensure(Commons, will_extract_only_path_if_there_is_one) {
+    char *source = "path/file";
     char dest[100];
-    char source[100];
     int length;
 
-    strcpy(source, "path/file");
     length = extractPathInto(source, dest);
     assert_that(dest, is_equal_to_string("path/")); /* Copy only path */
     assert_that(length, is_equal_to(strlen("path/")));
