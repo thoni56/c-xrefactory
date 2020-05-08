6639b4828e143fe6296d5d40992b8450e1ad55b1
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2020-05-08 12:40:22 +0200

    Describe yystype.h in a comment

--- src/yystype.h
+++ src/yystype.h
@@ -1,23 +1,31 @@
-    Symbol                                 *symbol;
-    TypeModifier                           *typeModifier;
-    S_freeTrail                            *trail;
+/* -*- c -*-*/
 
-    S_extRecFindStr                        *erfs;
+/* This are the parser/yacc fields required. They should be the same
+   for all parsers since the YYSTYPE need to be the same. Include this
+   file in the %union section instead of local field declarations and
+   make additions here.
+*/
 
-    Ast_int                                ast_integer;
-    Ast_unsigned                           ast_unsigned;
-    Ast_symbol                             ast_symbol;
-    Ast_symbolList                         ast_symbolList;
-    Ast_typeModifiers                      ast_typeModifiers;
-    Ast_id                                 ast_id;
-    Ast_idList                             ast_idList;
-    Ast_exprTokenType                      ast_expressionType;
-    Ast_intPair                            ast_intPair;
-    Ast_whileExtractData                   ast_whileData;
-    Ast_position                           ast_position;
-    Ast_unsPositionPair                    ast_unsignedPositionPair;
-    Ast_symbolPositionPair                 ast_symbolPositionPair;
-    Ast_symbolPositionListPair             ast_symbolPositionListPair;
-    Ast_positionList                       ast_positionList;
-    Ast_typeModifiersListPositionListPair  ast_typeModifiersListPositionListPair;
-    Ast_nestedConstrTokenType              ast_nestedConstrTokenType;
+Symbol                                 *symbol;
+TypeModifier                           *typeModifier;
+S_freeTrail                            *trail;
+
+S_extRecFindStr                        *erfs;
+
+Ast_int                                ast_integer;
+Ast_unsigned                           ast_unsigned;
+Ast_symbol                             ast_symbol;
+Ast_symbolList                         ast_symbolList;
+Ast_typeModifiers                      ast_typeModifiers;
+Ast_id                                 ast_id;
+Ast_idList                             ast_idList;
+Ast_exprTokenType                      ast_expressionType;
+Ast_intPair                            ast_intPair;
+Ast_whileExtractData                   ast_whileData;
+Ast_position                           ast_position;
+Ast_unsPositionPair                    ast_unsignedPositionPair;
+Ast_symbolPositionPair                 ast_symbolPositionPair;
+Ast_symbolPositionListPair             ast_symbolPositionListPair;
+Ast_positionList                       ast_positionList;
+Ast_typeModifiersListPositionListPair  ast_typeModifiersListPositionListPair;
+Ast_nestedConstrTokenType              ast_nestedConstrTokenType;
