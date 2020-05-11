82fee12aaa7dc0b888865cc72492ea54df506883
Thomas Nilefalk <thomas@nilefalk.se>
2020-05-11 20:24:05 +0200

    Make .dir-locals.el correct

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,10 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(add-to-list 'auto-mode-alist '("\\.c\\'" . cov-mode))
 ((c-mode . (
             (indent-tabs-mode . nil)
             (c-basic-offset . 4)
+            (mode . cov)
             )
          )
  )
