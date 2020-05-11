9e72a7cca2c4e78f1176c0457d885c2c644620ac
Thomas Nilefalk <thomas@nilefalk.se>
2020-05-11 19:08:32 +0200

    Add cov-mode to alist for .c

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
+(add-to-list 'auto-mode-alist '("\\.c\\'" . cov-mode))
 ((c-mode . (
             (indent-tabs-mode . nil)
             (c-basic-offset . 4)
