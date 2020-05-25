0c889a1e86f6d726f6f5fe7d99c18603df4968c0
Thomas Nilefalk <thomas@nilefalk.se>
2020-05-25 08:22:35 +0200

    Add exercising tests for operators to get coverage in lex.c

---
+++ tests/test_lex_operators/operators.c
@@ -0,0 +1,20 @@
+int integer;
+
+integer = 1;
+integer += 1;
+integer++;
+integer>>1;
+integer>=4;
+integer==5;
+integer>>1;
+integer>=4;
+integer<<=2;
+integer>>=2;
+integer*=12;
+integer%=2;
+integer&*c;
+integer^=42;
+integer/=2;
+// comment continuing on new line \
+here too
+\
