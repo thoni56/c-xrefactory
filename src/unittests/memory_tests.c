e09b4cb283f1dcadb8cb80ab8cc4b19bdc44ebbe
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-02 23:56:28 +0200

    Add an empty unittest for memory.c

---
+++ src/unittests/memory_tests.c
@@ -0,0 +1,13 @@
+#include <cgreen/cgreen.h>
+
+#include "memory.h"
+#include "globals.mock"
+
+
+Describe(Memory);
+BeforeEach(Memory) {}
+AfterEach(Memory) {}
+
+
+Ensure(Memory, can_run_an_empty_test) {
+}
