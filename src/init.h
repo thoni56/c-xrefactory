dc57103d192e3e0d3ee4b67cf45c09113a403eea
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-10 17:54:03 +0200

    Remove extern declation of non-existing function

--- src/init.h
+++ src/init.h
@@ -6,7 +6,6 @@ extern void initJavaTypePCTIConvertIniTab(void);
 extern void initTypeCharCodeTab(void);
 extern void initArchaicTypes(void);
 extern void initPreCreatedTypes(void);
-extern void initTypeModifiersTabs(void);
 extern void initExtractStoragesNameTab(void);
 extern void initTypesNamesTab(void);
 
