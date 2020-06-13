d0b3f5f8f773266c5d234863ce5a3319cc7f4d3a
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-13 10:52:26 +0200

    Add c-offset setting to .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,12 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode . (
+(
+ (c-mode . (
             (indent-tabs-mode . nil)
+            (c-file-style . "k&r")
             (c-basic-offset . 4)
-            (c-default-style . "k&r")
+            (eval . (c-set-offset 'arglist-close '0))
             (mode . cov)
             )
          )
