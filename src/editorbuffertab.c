d1b486f2d2c5533253ded5c64edb923282d7791b
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-14 10:44:33 +0200

    Create new version of hashlist.t[ch] to be able to define type names

--- src/editorbuffertab.c
+++ src/editorbuffertab.c
@@ -3,12 +3,12 @@
 
 #include "hash.h"
 
-#include "misc.h"               /* For XX_ALLOCC */
+#include "memory.h"               /* For XX_ALLOCC */
 
 #define HASH_FUN(elemp) hashFun(elemp->f->name)
 #define HASH_ELEM_EQUAL(e1,e2) (strcmp(e1->f->name,e2->f->name)==0)
 
-#include "hashlist.tc"
+#include "hashlist-new.tc"
 
 S_editorBufferList *s_staticEditorBufferTabTab[EDITOR_BUFF_TAB_SIZE];
-S_editorBufferTab s_editorBufferTab;
+EditorBufferTab s_editorBufferTab;
--- src/editorbuffertab.h
+++ src/editorbuffertab.h
@@ -4,18 +4,20 @@
 #include "editor.h"
 
 #define HASH_TAB_NAME editorBufferTab
+#define HASH_TAB_TYPE EditorBufferTab
 #define HASH_ELEM_TYPE S_editorBufferList
 
-#include "hashlist.th"
+#include "hashlist-new.th"
 
 #ifndef _EDITORBUFFERTAB_
 #undef HASH_TAB_NAME
+#undef HASH_TAB_TYPE
 #undef HASH_ELEM_TYPE
 #endif
 
 #define EDITOR_BUFF_TAB_SIZE 100
 
 extern S_editorBufferList *s_staticEditorBufferTabTab[];
-extern S_editorBufferTab s_editorBufferTab;
+extern EditorBufferTab s_editorBufferTab;
 
 #endif
