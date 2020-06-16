188ea5ca4b31dce2e9cb3a03d505482fb9fa1e8c
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-16 07:49:46 +0200

    Add octal number to simple C parsing test

--- tests/test_c_parsing/test_source/hello.c
+++ tests/test_c_parsing/test_source/hello.c
@@ -4,6 +4,6 @@ int main(int argc, char **argv) {
     char str[50];
     struct s {int a; float f; char c; char *p;} s;
     s = (struct s){.a = 3, .f = 3.14, .c = 'c', .p = str};
-    sprintf(str, "Hello");
+    sprintf(str, "Hello", 012345678);
     strcat(str, "World");
 }
