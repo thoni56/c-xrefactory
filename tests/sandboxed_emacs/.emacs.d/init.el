fd2e876ac50682976aa109d282f8189588f1239b
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-25 20:25:05 +0200

    Add safe-variables to sandboxed emacs

--- tests/sandboxed_emacs/.emacs.d/init.el
+++ tests/sandboxed_emacs/.emacs.d/init.el
@@ -66,3 +66,19 @@
 
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((eval c-set-offset
+           (quote arglist-close)
+           (quote 0))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
