a466a86f6d23e8c1459f4afefda14d0177dde7ae
Thomas Nilefalk <thomas@nilefalk.se>
2020-07-10 18:58:51 +0200

    Add script to convert c-xref.el debug messages to input to edit_server_driver.py

---
+++ utils/messages2commands.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import sys
+import re
+
+def fixup_calling(line):
+    _, line = line.split("calling: (")
+    line, _ = line.split(")")
+    line = re.sub(" \"-o\" \"[^\"]*\"", '', line)
+    print("../../src/c-xref", line)
+
+def fixup_sending(line):
+    _, line = line.split("sending: ")
+    print(line[:-1])
+    print("<sync>")
+
+with open(sys.argv[1], "r") as messages:
+    for line in messages:
+        if line.startswith("calling:"):
+            fixup_calling(line)
+        if line.startswith("sending:"):
+            fixup_sending(line)
+print("<exit>")
