48481a1c64f1589f627934de166e8e17007ad122
Thomas Nilefalk <thomas@nilefalk.se>
2020-07-12 07:27:18 +0200

    Minor reformats

--- src/options.h
+++ src/options.h
@@ -5,15 +5,15 @@
 #include "stdbool.h"
 
 extern void addSourcePathsCut(void);
-extern void getXrefrcFileName( char *ttt );
+extern void getXrefrcFileName(char *ttt);
 extern char *getJavaHome(void);
 extern void getJavaClassAndSourcePath(void);
 extern int packageOnCommandLine(char *fn);
 extern char *expandSpecialFilePredefinedVariables_st(char *tt);
 extern bool readOptionFromFile(FILE *ff, int *nargc, char ***nargv,
-                              int memFl, char *sectionFile, char *project, char *resSection);
-extern void readOptionFile(char *name, int *nargc, char ***nargv,char *sectionFile, char *project);
-extern void readOptionPipe(char *command, int *nargc, char ***nargv,char *sectionFile);
+                               int memFl, char *sectionFile, char *project, char *resSection);
+extern void readOptionFile(char *name, int *nargc, char ***nargv, char *sectionFile, char *project);
+extern void readOptionPipe(char *command, int *nargc, char ***nargv, char *sectionFile);
 extern void javaSetSourcePath(int defaultCpAllowed);
 extern int changeRefNumOption(int newRefNum);
 
