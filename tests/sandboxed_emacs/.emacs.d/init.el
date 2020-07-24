49434a117e05750ffcfa9290359c785f3a521aa1
Thomas Nilefalk <thomas@nilefalk.se>
2020-07-24 10:57:29 +0200

    Simplify sandboxed emacs somewhat

--- tests/sandboxed_emacs/.emacs.d/init.el
+++ tests/sandboxed_emacs/.emacs.d/init.el
@@ -4,68 +4,14 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(when (eq system-type 'cygwin)
-  ;; For native Cygwin Emacs-w32 we want to use login shells
-  (setenv "PATH" (shell-command-to-string "source ~/.bash_profile; echo -n $PATH"))
-  (setenv "PATH" (concat "/usr/bin" path-separator
-                       (getenv "PATH")))
-  (setq shell-file-name "bash")
-  (setenv "SHELL" shell-file-name)
-  (setq explicit-shell-file-name shell-file-name)
-
-  (setq explicit-bash-args '("--login" "-i"))
-  (setq exec-path (cons "/bin" exec-path))
-  )
-
 ;;; El-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+(require 'el-get)
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
 
-;;; Add some Melpa archive
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/el-get/c-xrefactory/env/emacs")
+(load-file "~/.emacs.d/el-get/c-xrefactory/env/emacs/c-xrefactory.el")
 
-;;; Formatting and indentation
-(setq-default tab-width 4)
-(setq c-basic-offset 4)
-(setq c-tab-always-indent t)
-(setq-default indent-tabs-mode nil)
-
-(add-hook 'before-save-hook 'whitespace-cleanup)
-
-;; Compilation keys
-(global-set-key [f10] 'compile)
-(global-set-key [f9] 'next-error)
-(setq compilation-scroll-output t)
-
-
-(global-set-key [kp-delete] 'delete-char)
-(if (eq system-type 'darwin)
-    (setq mac-option-modifier nil
-      mac-command-modifier 'meta
-      x-select-enable-clipboard t))
-
-;; Colorize compilation buffer
-(ignore-errors
-  (require 'ansi-color)
-  (defun my-colorize-compilation-buffer ()
-    (when (eq major-mode 'compilation-mode)
-      (ansi-color-apply-on-region compilation-filter-start (point-max))))
-  (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
-
-;; Extra Makefile-mode pattern
-(setq auto-mode-alist
-        (cons '("Makefile\\.*" . makefile-mode) auto-mode-alist))
-
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -74,8 +20,8 @@
  '(safe-local-variable-values
    (quote
     ((eval c-set-offset
-           (quote arglist-close)
-           (quote 0))))))
+       (quote arglist-close)
+       (quote 0))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
