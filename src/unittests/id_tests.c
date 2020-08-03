c16b510e518688d7f0fac0834171b33a999b112f
Thomas Nilefalk <thomas@nilefalk.se>
2020-08-03 07:50:50 +0200

    Add unittest for id.c

---
+++ src/unittests/id_tests.c
@@ -0,0 +1,29 @@
+#include <cgreen/cgreen.h>
+
+#include "id.h"
+
+/* Dependencies: */
+#include "memory.mock"
+
+
+Describe(Id);
+BeforeEach(Id) {}
+AfterEach(Id) {}
+
+
+Ensure(Id, can_copy_id) {
+    Id id;
+    Id *id1 = &id;
+    Id *id2;
+
+    id1->name = "idName";
+    id1->p.file = 1;
+    id1->p.line = 2;
+    id1->p.col = 3;
+    id1->symbol = NULL;
+    id1->next = &id;
+
+    id2 = newCopyOfId(id1);
+
+    assert_that(memcmp(id1, id2, sizeof(Id)), is_equal_to(0));
+}
