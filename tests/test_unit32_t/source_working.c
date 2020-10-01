1d68bb1cbaac295f8dfd6e88fd3f7dbd6be48a6a
Thomas Nilefalk <thomas@nilefalk.se>
2020-10-01 07:51:36 +0200

    Change up uint32_t test to look more like <stdint.h>

--- tests/test_unit32_t/source_working.c
+++ tests/test_unit32_t/source_working.c
@@ -1,6 +1,6 @@
-#define __UINT32_TYPE__X unsigned int
-#ifdef __UINT32_TYPE__X
-typedef __UINT32_TYPE__X uint32_t;
+#include <stdint.h>
+#ifdef __UINT32_TYPE__
+typedef __UINT32_TYPE__ uint32_t;
 #endif
 
 typedef uint32_t Aint;
