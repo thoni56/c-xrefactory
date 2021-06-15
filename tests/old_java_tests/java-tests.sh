6a9f567fb662e21f8ddfe7c2dd7d7e064dab8b71
Thomas Nilefalk <thomas@nilefalk.se>
2021-06-15 19:33:54 +0200

    [tests][legacy] Fixup old java tests script (not working anyway...)

--- tests/old_java_tests/java-tests.sh
+++ tests/old_java_tests/java-tests.sh
@@ -2,11 +2,12 @@ cp -r ../doc/jexercise /tmp
 
 export SOURCEPATH=/tmp/jexercise
 export CXREFRC="-xrefrc /tmp/jexercise/.c-xrefrc -p /tmp/jexercise"
+export CXREF=../../src/c-xref
 
-./c-xref -refactory -rfct-add-param -olcxparnum=1 "-rfct-param1=int i;" -rfct-param2=0 $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
+$CXREF -refactory -rfct-add-param -olcxparnum=1 "-rfct-param1=int i;" -rfct-param2=0 $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
 
-./c-xref -refactory -rfct-del-param -olcxparnum=1 -xrefrc $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
+$CXREF -refactory -rfct-del-param -olcxparnum=1 -xrefrc $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
 
-./c-xref -refactory -rfct-move-param -olcxparnum=1 -olcxparnum2=2 $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
+$CXREF -refactory -rfct-move-param -olcxparnum=1 -olcxparnum2=2 $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
 
-./c-xref -refactory -rfct-rename -renameto=tototo $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
+$CXREF -refactory -rfct-rename -renameto=tototo $CXREFRC -olcursor=866 /tmp/jexercise/com/xrefactory/refactorings/MoveMethod.java
