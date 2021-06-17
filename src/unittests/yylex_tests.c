ca04877bd5affc8f36cacf7fabcf0bd7de9c3e70
Thomas Nilefalk <thomas@nilefalk.se>
2021-06-17 09:32:00 +0200

    [tests][include] Add custom constraint to verify filename in test

--- src/unittests/yylex_tests.c
+++ src/unittests/yylex_tests.c
@@ -89,10 +89,51 @@ Ensure(Yylex, can_handle_a_line_directive_without_number) {
     /* No asserts, only for execution of END_OF_FILE_EXCEPTION in #line directive */
 }
 
+/* ----------------------------------------------------*/
+/* Custom Cgreen constraint to capture parameter value */
+
+/* We need a copy of ... */
+
+static CgreenValue make_cgreen_pointer_value(void *pointer) {
+    CgreenValue value = {CGREEN_POINTER, {0}, sizeof(intptr_t)};
+    value.value.pointer_value = pointer;
+    return value;
+}
+
+
+static void capture_parameter(Constraint *constraint, const char *function, CgreenValue actual,
+                              const char *test_file, int test_line, TestReporter *reporter) {
+    (void)function;
+    (void)test_file;
+    (void)test_line;
+    (void)reporter;
+
+    memmove(constraint->expected_value.value.pointer_value, &actual.value, constraint->size_of_expected_value);
+}
+
+static bool compare_true() { return true; }
+
+static Constraint *create_capture_parameter_constraint(const char *parameter_name, void *capture_to, size_t size_to_capture) {
+    Constraint* constraint = create_constraint();
+
+    constraint->compare = &compare_true;
+    constraint->execute = &capture_parameter;
+    constraint->name = "capture parameter";
+    constraint->expected_value = make_cgreen_pointer_value(capture_to);
+    constraint->size_of_expected_value = size_to_capture;
+    constraint->parameter_name = parameter_name;
+
+    return constraint;
+}
+
+#define will_capture_parameter(parameter_name, local_variable) create_capture_parameter_constraint(#parameter_name, &local_variable, sizeof(local_variable))
+
+
 Ensure(Yylex, can_process_include_directive) {
     Position ipos = (Position){1,2,3};
     char *lexem_stream = "\303\001\"include.h";
     FILE file;
+    int fileNumber;
 
     strcpy(cwd, "cwd");
 
@@ -106,16 +147,20 @@ Ensure(Yylex, can_process_include_directive) {
     expect(extractPathInto, will_set_contents_of_parameter(dest, "some/path", 10));
     always_expect(normalizeFileName,
                   will_return("some/path/include.h"));
-    expect(editorFindFile, will_return(NULL));
+    expect(editorFindFile,
+           when(name, is_equal_to_string("some/path/include.h")),
+           will_return(NULL));
     expect(openFile, when(fileName, is_equal_to_string("some/path/include.h")), will_return(&file));
 
     /* Always  */
     always_expect(checkFileModifiedTime);
-    always_expect(cacheInclude);
+    always_expect(cacheInclude, will_capture_parameter(fileNum, fileNumber));
 
     /* Finally ensure that the include file is added as a reference */
     expect(addCxReference, when(symbol_name, is_equal_to_string(LINK_NAME_INCLUDE_REFS)));
     expect(addCxReference, when(symbol_name, is_equal_to_string(LINK_NAME_INCLUDE_REFS)));
 
     processIncludeDirective(&ipos);
+
+    assert_that(fileTable.tab[fileNumber]->name, is_equal_to_string("some/path/include.h"));
 }
