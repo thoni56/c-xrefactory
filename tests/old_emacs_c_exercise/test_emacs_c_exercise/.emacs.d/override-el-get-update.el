20ad6b4138d9c9f83183566304b95c3d745101ed
Thomas Nilefalk <thomas@nilefalk.se>
2021-09-05 22:48:01 +0200

    [tests][cleanup] Remove old tests and rename legacy tests

--- tests/old_emacs_c_exercise/test_emacs_c_exercise/.emacs.d/override-el-get-update.el
+++
@@ -1,4 +0,0 @@
-;; Override el-gets update functions
-(defun el-get-update () (message "You cannot update el-get packages in this sandboxed environment"))
-(defun el-get-update-all () (message "You cannot update el-get packages in this sandboxed environment"))
-(defun el-get-update-packages-of-type () (message "You cannot update el-get packages in this sandboxed environment"))
