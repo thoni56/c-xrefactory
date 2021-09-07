2b6a9e1cf0fbaf7f479fe591b1ab4632f2c0ba87
Thomas Nilefalk <thomas@nilefalk.se>
2021-09-07 21:46:57 +0200

    [travis][stable] Fix moving stable now that we are on main

--- .travis.merge_to_stable_maybe.sh
+++ .travis.merge_to_stable_maybe.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "We're not building on the master branch. Not moving stable branch."
+if [[ "$TRAVIS_BRANCH" != "main" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "We're not building on the main branch. Not moving stable branch."
   exit 0
 fi
 
-echo "Moving stable branch to current master..."
+echo "Moving stable branch to current main..."
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git push -f https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG master:refs/heads/stable
+git push -f https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG main:refs/heads/stable
 echo TAGPERM=$TAGPERM
 echo TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG
