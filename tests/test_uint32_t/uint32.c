03de245118c929865a7fc7a214a58c900e00a5b1
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2021-09-14 12:35:06 +0200

    [c][stddefs] First step to make test_uint32 into a C99 checker

--- tests/test_uint32_t/uint32.c
+++ tests/test_uint32_t/uint32.c
@@ -1,2 +1,39 @@
 #include <stdint.h>
-uint32_t x;
+
+typedef int8_t a;
+typedef int16_t a;
+typedef int32_t a;
+typedef int64_t a;
+ 
+typedef int_fast8_t a;
+typedef int_fast16_t a;
+typedef int_fast32_t a;
+typedef int_fast64_t a;
+ 
+typedef int_least8_t a;
+typedef int_least16_t a;
+typedef int_least32_t a;
+typedef int_least64_t a;
+ 
+typedef intmax_t a;
+
+typedef intptr_t a;
+ 
+typedef uint8_t a;
+typedef uint16_t a;
+typedef uint32_t a;
+typedef uint64_t a;
+ 
+typedef uint_fast8_t a;
+typedef uint_fast16_t a;
+typedef uint_fast32_t a;
+typedef uint_fast64_t a;
+ 
+typedef uint_least8_t a;
+typedef uint_least16_t a;
+typedef uint_least32_t a;
+typedef uint_least64_t a;
+ 
+typedef uintmax_t a;
+ 
+typedef uintptr_t a;
