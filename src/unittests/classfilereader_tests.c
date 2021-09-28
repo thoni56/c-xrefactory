6b204a3f59bc2f14e3e253e6dd465bdf3e1a48bd
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2021-09-28 16:53:09 +0200

    [unittests][module] Get classfilereader under unittests for compilation only

---
+++ src/unittests/classfilereader_tests.c
@@ -0,0 +1,32 @@
+#include <cgreen/cgreen.h>
+
+#include "classfilereader.h"
+#include "log.h"
+
+#include "globals.mock"
+#include "commons.mock"
+#include "misc.mock"
+#include "cxref.mock"
+#include "filetable.mock"
+#include "options.mock"
+#include "yylex.mock"
+#include "editor.mock"
+#include "memory.mock"
+#include "characterreader.mock"
+#include "fileio.mock"
+#include "symbol.mock"
+#include "classcaster.mock"
+#include "jsemact.mock"
+#include "filedescriptor.mock"
+#include "typemodifier.mock"
+#include "cxfile.mock"
+
+
+Describe(Classfilereader);
+BeforeEach(Classfilereader) {
+    log_set_level(LOG_DEBUG); /* Set to LOG_TRACE if needed */
+}
+AfterEach(Classfilereader) {}
+
+Ensure(Classfilereader, can_run_empty_test) {
+}
