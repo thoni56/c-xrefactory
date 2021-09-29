55e660dc6ac08deb0c9d196258b734df2277c9c7
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2021-09-29 14:21:38 +0200

    [code][bool] Boolify creatingOlcxRefs()

--- src/utils.h
+++ src/utils.h
@@ -6,7 +6,7 @@
 /* Complement log.c with function to explicitly give file & line */
 #define log_with_explicit_file_and_line(level, file, line, ...) log_log(level, file, line, __VA_ARGS__)
 
-extern int creatingOlcxRefs(void);
+extern bool creatingOlcxRefs(void);
 extern void recursivelyCreateFileDirIfNotExists(char *fpath);
 
 #endif
