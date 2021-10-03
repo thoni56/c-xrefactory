c8ed5095f01a396bcf95e0442ed3d0e54c5adefd
Thomas Nilefalk <thomas@nilefalk.se>
2021-10-03 10:50:22 +0200

    [unittests][module] Get caching under (empty) unittest for compilation

---
+++ src/unittests/caching_tests.c
@@ -0,0 +1,24 @@
+#include <cgreen/cgreen.h>
+
+#include "../caching.h"
+
+#include "yylex.mock"
+#include "globals.mock"
+#include "options.mock"
+#include "reftab.mock"
+#include "javafqttab.mock"
+#include "filedescriptor.mock"
+#include "filetable.mock"
+#include "symboltable.mock"
+#include "jsemact.mock"
+#include "editor.mock"
+#include "commons.mock"
+
+
+Describe(Caching);
+BeforeEach(Caching) {}
+AfterEach(Caching) {}
+
+
+Ensure(Caching, can_run_empty_test) {
+}
