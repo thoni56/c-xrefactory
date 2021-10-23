ecbf0780837497b83d21d647fcf558facb21fb88
Thomas Nilefalk <thomas@nilefalk.se>
2021-10-23 09:24:38 +0200

    [unittests][classhierarchy] Add empty unittests

---
+++ src/unittests/classhierarchy_tests.c
@@ -0,0 +1,26 @@
+#include <cgreen/cgreen.h>
+
+#include "classhierarchy.h"
+#include "log.h"
+#include "protocol.h"
+
+#include "memory.mock"
+#include "options.mock"
+#include "globals.mock"
+#include "filetable.mock"
+#include "type.mock"
+#include "misc.mock"
+#include "cxref.mock"
+#include "ppc.mock"
+#include "commons.mock"
+
+
+Describe(ClassHierarchy);
+BeforeEach(ClassHierarchy) {
+    log_set_level(LOG_ERROR);
+}
+AfterEach(ClassHierarchy) {}
+
+
+Ensure(ClassHierarchy, can_run_emtpy_test) {
+}
