5ed2d2961ce5d260135e789fd0b75e04aef66aea
Thomas Nilefalk <thomas@nilefalk.se>
2021-11-07 21:07:49 +0100

    [misc] Some TODOs, comment in messages2commands.py and checkmark

--- utils/messages2commands.py
+++ utils/messages2commands.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python3
 
+# This python script can take a message log from c-xref-debug-mode and
+# convert that to a usable commands.input to send to server-driver.py
+# to drive the c-xrefactory server.
+
+# It will replace any occurrence of current directory with CURDIR, so it
+# is handy to be in the directory where the processed files are located.
+
 import sys
 import re
+import os
 
 def remove_preload(line):
     if "-preload" in line:
@@ -9,11 +17,16 @@ def remove_preload(line):
     else:
         return line
 
+def replace_curdir(line):
+    cwd = os.getcwd()
+    return line.replace(cwd, "CURDIR")
+
 def fixup_calling(line):
     _, line = line.split("calling: (")
     line, _ = line.split(")")
     line = re.sub(" \"-o\" \"[^\"]*\"", '', line)
     line = remove_preload(line)
+    line = replace_curdir(line)
     print("../../src/c-xref", line)
 
 def fixup_sending(line):
