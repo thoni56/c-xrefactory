2a571df29513faa3171c0450c91a417d0172bcec
Thomas Nilefalk <thomas@nilefalk.se>
2022-03-07 07:26:25 +0100

    [cxfile][unittest] Add some expects for initial line read

--- src/cxfile_tests.c
+++ src/cxfile_tests.c
@@ -1,6 +1,7 @@
 #include <cgreen/assertions.h>
 #include <cgreen/cgreen.h>
 #include <cgreen/constraint_syntax_helpers.h>
+#include <cgreen/mocks.h>
 
 #include "characterreader.h"
 #include "cxfile.h"
@@ -42,13 +43,25 @@ xEnsure(CxFile, can_do_normal_scan) {
     FILE *filePointer = (FILE *)4654654645;
     CharacterBuffer *buffer;
 
+    cgreen_mocks_are(learning_mocks);
     options.cxrefsLocation = "./CXrefs";
 
     expect(openFile, when(fileName, is_equal_to_string("./CXrefs/XFiles")), will_return(filePointer));
     expect(initCharacterBuffer, when(file, is_equal_to(filePointer)),
            will_capture_parameter(characterBuffer, buffer));
 
-    always_expect(skipWhiteSpace);
+    /* Version marking always starts a file */
+    expect(skipWhiteSpace, will_return('3'));
+    expect(getChar, will_return('4'));
+    expect(getChar, will_return('v'));
+    expect(getChar, will_return(' '));
+    expect(skipCharacters, when(count, is_equal_to(33)));
+
+    /* Generation setttings, file count, ... */
+    expect(getChar, will_return('2'));
+    expect(getChar, will_return('1'));
+    expect(getChar, will_return('@'));
+    expect(getChar, will_return(' '));
 
     normalScanReferenceFile("/XFiles");
 }
