75ddc0b88d768f0af7d346cc23269b8d51720902
Thomas Nilefalk <thomas@nilefalk.se>
2022-04-04 23:17:59 +0200

    [build][stable] Set and push a new tag "previous_stable" for reverting

--- .travis_merge_to_stable_maybe.sh
+++ .travis_merge_to_stable_maybe.sh
@@ -9,6 +9,8 @@ echo "Moving stable branch to current main..."
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
+git tag -f previous_stable stable
 git push -f https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG main:refs/heads/stable
+git push -f https://$TAGPERM@github.com/$TRAVIS_REPO_SLUG main:refs/heads/previous_stable
 echo TAGPERM=$TAGPERM
 echo TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG
