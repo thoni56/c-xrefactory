4fb517ce1e53cdf2272b1184943baaaa789c9561
Thomas Nilefalk <thomas@nilefalk.se>
2022-06-24 20:23:53 +0200

    [unittests][characterreader] Add test for ungetChar()

--- src/characterreader_tests.c
+++ src/characterreader_tests.c
@@ -5,11 +5,14 @@
 
 #include "commons.mock"
 #include "fileio.mock"
+#include "log.h"
 
 
 
 Describe(CharacterReader);
-BeforeEach(CharacterReader) {}
+BeforeEach(CharacterReader) {
+    log_set_level(LOG_ERROR);
+}
 AfterEach(CharacterReader) {}
 
 
@@ -22,4 +25,18 @@ Ensure(CharacterReader, can_get_string) {
     getString(read_string, strlen("the string"), &cb);
 
     assert_that(read_string, is_equal_to_string("the string"));
+}
+
+Ensure(CharacterReader, can_read_and_unread) {
+    CharacterBuffer cb;
+
+    initCharacterBufferFromString(&cb, "some string");
+
+    int ch = getChar(&cb);
+
+    assert_that(ch, is_equal_to('s'));
+    assert_that(cb.nextUnread, is_equal_to(cb.chars+1));
+
+    ungetChar(&cb, ch);
+    assert_that(cb.nextUnread, is_equal_to(cb.chars));
 }
