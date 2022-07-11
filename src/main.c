dc9b9270a270355411df19d8a817fe902c11641a
Thomas Nilefalk <thomas@nilefalk.se>
2022-07-11 07:43:16 +0200

    [tidy][about] Change about-message to conform to GNU standard

--- src/main.c
+++ src/main.c
@@ -54,7 +54,8 @@ static int oldPass;
 
 static void aboutMessage(void) {
     char output[REFACTORING_TMP_STRING_SIZE];
-    sprintf(output, "This is C-xrefactory version %s compiled at %s on %s\n", C_XREF_VERSION_NUMBER, __TIME__, __DATE__);
+    sprintf(output, "C-xrefactory version %s\n", C_XREF_VERSION_NUMBER);
+    sprintf(output, "Compiled at %s on %s\n",  __TIME__, __DATE__);
     sprintf(output+strlen(output), "from git revision %s.\n", GIT_HASH);
     sprintf(output+strlen(output), "(c) 1997-2004 by Xref-Tech, http://www.xref-tech.com\n");
     sprintf(output+strlen(output), "Released into GPL 2009 by Marian Vittek (SourceForge)\n");
