275425cde25a7ef8a2e1f9212e9dcd05d38b71b2
Thomas Nilefalk <thomas@nilefalk.se>
2022-07-13 16:10:24 +0200

    [tidy][modules] Starting to break out server from main.c

--- src/server.c
+++ src/server.c
@@ -1,5 +1,11 @@
 #include "server.h"
 
+#include "cxref.h"
+#include "options.h"
+#include "complete.h"
+#include "globals.h"
+#include "caching.h"
+
 const char *operationNamesTable[] = {
     ALL_OPERATION_ENUMS(GENERATE_ENUM_STRING)
 };
