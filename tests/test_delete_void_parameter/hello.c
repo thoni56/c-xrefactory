f963cceff08f88bbb65bc04eadf00584eae6b91b
Thomas Nilefalk <thomas@nilefalk.se>
2022-08-03 23:40:30 +0200

    [test][simplify] Remove stdio for simplification

--- tests/test_delete_void_parameter/hello.c
+++ tests/test_delete_void_parameter/hello.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
-
 static void function(void) {
+    /* Reductions by:
+       type_specifier1 : void
+       declaration_specifiers0 : type_specifier1
+       declaration_specifiers : declaration_specifiers0
+       type_name : declaration_specifiers
+       parameter_declaration : type_name
+       parameter_list : parameter_declaration
+       parameter_type_list : parameter_list
+    */
 }
 
 
