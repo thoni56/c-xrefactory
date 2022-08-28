2ed9d204246ea3ef6322059b9b19e724b2b48008
Thomas Nilefalk <thomas@nilefalk.se>
2022-08-28 15:59:28 +0200

    [characterreader][unittests] Add unittests for skipWhiteSpace()

--- src/characterreader_tests.c
+++ src/characterreader_tests.c
@@ -13,20 +13,20 @@ BeforeEach(CharacterReader) {
 }
 AfterEach(CharacterReader) {}
 
+static CharacterBuffer cb;
+
 Ensure(CharacterReader, can_get_string) {
-    CharacterBuffer cb;
     char            read_string[100];
+    char            expected_string[] = "the string";
 
-    initCharacterBufferFromString(&cb, "the string");
+    initCharacterBufferFromString(&cb, expected_string);
 
-    getString(read_string, strlen("the string"), &cb);
+    getString(read_string, strlen(expected_string), &cb);
 
-    assert_that(read_string, is_equal_to_string("the string"));
+    assert_that(read_string, is_equal_to_string(expected_string));
 }
 
 Ensure(CharacterReader, can_read_and_unread) {
-    CharacterBuffer cb;
-
     initCharacterBufferFromString(&cb, "some string");
 
     int ch = getChar(&cb);
@@ -36,4 +36,15 @@ Ensure(CharacterReader, can_read_and_unread) {
 
     ungetChar(&cb, ch);
     assert_that(cb.nextUnread, is_equal_to(cb.chars));
+}
+
+Ensure(CharacterReader, can_skip_whitespace) {
+    char string_starting_with_whitespace[] = " \t \nnomorewhitespace";
+
+    initCharacterBufferFromString(&cb, string_starting_with_whitespace);
+
+    int ch = skipWhiteSpace(&cb, ' ');
+
+    assert_that(ch, is_equal_to('n'));
+    assert_that(cb.nextUnread, is_equal_to(&cb.chars[5]));
 }
