4c27630819394e2a0c2a4c2821efb4f14e04831a
Thomas Nilefalk <thomas@nilefalk.se>
2022-09-10 15:27:00 +0200

    [unittests][editor] Cover sortEditorRegionsAndRemoveOverlaps()

--- src/editor_tests.c
+++ src/editor_tests.c
@@ -57,3 +57,76 @@ Ensure(Editor, can_create_buffer_for_non_existing_file) {
     // Then...
     assert_that(buffer->name, is_equal_to_string("non-existant.c"));
 }
+
+
+Ensure(Editor, can_sort_empty_list_of_regions) {
+    EditorRegionList *list = NULL;
+    sortEditorRegionsAndRemoveOverlaps(&list);
+
+    assert_that(list, is_null);
+}
+
+Ensure(Editor, can_sort_single_region) {
+    EditorRegionList regionList = {.next = NULL};
+    EditorRegionList *list = &regionList;
+
+    sortEditorRegionsAndRemoveOverlaps(&list);
+
+    assert_that(list, is_equal_to(&regionList));
+}
+
+static EditorRegionList *createEditorRegionList(EditorBuffer *buffer, int begin, int end, EditorRegionList *next) {
+    EditorMarker *b = (EditorMarker *)malloc(sizeof(EditorMarker));
+    EditorMarker *e = (EditorMarker *)malloc(sizeof(EditorMarker));
+    *b = (EditorMarker){ .offset = begin, .previous = NULL, .next = NULL, .buffer = buffer };
+    *e = (EditorMarker){ .offset = end, .previous = NULL, .next = NULL, .buffer = buffer };
+
+    EditorRegionList *r = (EditorRegionList *)malloc(sizeof(EditorRegionList));
+    *r = (EditorRegionList){ .region = { .begin = b, .end = e}, .next = next};
+    return r;
+}
+
+Ensure(Editor, can_sort_two_regions) {
+    EditorBuffer *buffer = (EditorBuffer *)malloc(sizeof(EditorBuffer));
+    EditorRegionList *regionList1 = createEditorRegionList(buffer, 1, 2, NULL);
+    EditorRegionList *regionList2 = createEditorRegionList(buffer, 3, 4, regionList1);
+    EditorRegionList *list = regionList2;
+
+    sortEditorRegionsAndRemoveOverlaps(&list);
+
+    assert_that(list, is_equal_to(regionList1));
+}
+
+Ensure(Editor, can_sort_three_regions) {
+    EditorBuffer *buffer = (EditorBuffer *)malloc(sizeof(EditorBuffer));
+    EditorRegionList *regionList1 = createEditorRegionList(buffer, 1, 2, NULL);
+    EditorRegionList *regionList2 = createEditorRegionList(buffer, 3, 4, regionList1);
+    EditorRegionList *regionList3 = createEditorRegionList(buffer, 5, 6, regionList2);
+    EditorRegionList *list = regionList3;
+
+    sortEditorRegionsAndRemoveOverlaps(&list);
+
+    assert_that(list, is_equal_to(regionList1));
+}
+
+Ensure(Editor, can_sort_enclosing_regions) {
+    EditorBuffer *buffer = (EditorBuffer *)malloc(sizeof(EditorBuffer));
+    EditorRegionList *regionList1 = createEditorRegionList(buffer, 1, 4, NULL);
+    EditorRegionList *regionList2 = createEditorRegionList(buffer, 2, 3, regionList1);
+    EditorRegionList *list = regionList2;
+
+    sortEditorRegionsAndRemoveOverlaps(&list);
+
+    assert_that(list, is_equal_to(regionList1));
+}
+
+Ensure(Editor, can_sort_overlapping_regions) {
+    EditorBuffer *buffer = (EditorBuffer *)malloc(sizeof(EditorBuffer));
+    EditorRegionList *regionList1 = createEditorRegionList(buffer, 1, 3, NULL);
+    EditorRegionList *regionList2 = createEditorRegionList(buffer, 2, 4, regionList1);
+    EditorRegionList *list = regionList2;
+
+    sortEditorRegionsAndRemoveOverlaps(&list);
+
+    assert_that(list, is_equal_to(regionList1));
+}
