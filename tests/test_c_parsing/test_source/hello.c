76423cdd4f9fae6e71d974bae3a1f2a818ac9273
Thomas Nilefalk <thomas@nilefalk.se>
2022-09-12 21:41:09 +0200

    [tests][coverage] Add some unexpected lexem parsing for coverage

--- tests/test_c_parsing/test_source/hello.c
+++ tests/test_c_parsing/test_source/hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, ..) {
     char str[50];
     struct s {int a; float f; char c; char *p;} s;
     s = (struct s){.a = 3, .f = 3.14, .c = 'c', .p = str};
     sprintf(str, "Hello", 012345678);
     strcat(str, "World");
+    printf(.12);
 }
