e2a694bb937fb3f90e945646534c687bafa3a866
Thomas Nilefalk <thomas@nilefalk.se>
2023-01-02 15:29:22 +0100

    [travis] Use the latest cgreen (1.6.1) for side effects to execute last

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,5 +1,5 @@
-wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.0/cgreen_1.6.0_amd64.deb
-sudo apt-get install ./cgreen_1.6.0_amd64.deb
+wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.0/cgreen-1.6.1_amd64.deb
+sudo apt-get install ./cgreen-1.6.1_amd64.deb
 sudo apt install openjdk-8-jre
 lcov --version
 gcov --version
