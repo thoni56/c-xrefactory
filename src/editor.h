263499b4180f691ce30c17725ffce21a05ea02d0
Thomas Nilefalk <thomas@nilefalk.se>
2023-01-28 14:32:03 +0100

    [modules] Starting extract of EditorBuffer

--- src/editor.h
+++ src/editor.h
@@ -1,49 +1,28 @@
 #ifndef EDITOR_H_INCLUDED
 #define EDITOR_H_INCLUDED
 
+#include "editorbuffer.h"
+#include "usage.h"
+
+#include "reference.h"
+#include "completion.h"
+
 #include "proto.h"
+
 
 /* ***************** editor structures ********************** */
 
-typedef enum editorUndoOperations
-{
+typedef enum editorUndoOperation {
     UNDO_REPLACE_STRING,
     UNDO_RENAME_BUFFER,
     UNDO_MOVE_BLOCK,
-} EditorUndoOperations;
+} EditorUndoOperation;
 
 typedef enum {
     EDITOR_UNKNOWN,
     EDITOR_EMACS,
     EDITOR_JEDIT,
 } Editors;
-
-typedef struct {
-    int   bufferSize;
-    char *text;                 /* Where is the actual buffer? */
-    int   allocatedFreePrefixSize; /* How much space have we allocated *before* text? */
-    char *allocatedBlock;
-    int   allocatedIndex;
-    int   allocatedSize;
-} EditorBufferAllocationData;
-
-typedef struct editorBuffer {
-    char                      *name;
-    int                        fileNumber;
-    char                      *fileName;
-    time_t                     modificationTime;
-    bool                       textLoaded : 1;
-    bool                       modified : 1;
-    bool                       modifiedSinceLastQuasiSave : 1;
-    size_t                     size;
-    struct editorMarker       *markers;
-    EditorBufferAllocationData allocation;
-} EditorBuffer;
-
-typedef struct editorBufferList {
-    EditorBuffer            *buffer;
-    struct editorBufferList *next;
-} EditorBufferList;
 
 typedef struct editorMarker {
     EditorBuffer        *buffer;
@@ -53,24 +32,24 @@ typedef struct editorMarker {
 } EditorMarker;
 
 typedef struct editorMarkerList {
-    struct editorMarker     *marker;
-    struct usage             usage;
+    EditorMarker     *marker;
+    Usage             usage;
     struct editorMarkerList *next;
 } EditorMarkerList;
 
 typedef struct editorRegion {
-    struct editorMarker *begin;
-    struct editorMarker *end;
+    EditorMarker *begin;
+    EditorMarker *end;
 } EditorRegion;
 
 typedef struct editorRegionList {
-    struct editorRegion      region;
+    EditorRegion      region;
     struct editorRegionList *next;
 } EditorRegionList;
 
 typedef struct editorUndo {
     EditorBuffer             *buffer;
-    enum editorUndoOperations operation;
+    EditorUndoOperation operation;
     union editorUndoUnion {
         struct editorUndoStrReplace {
             unsigned offset;
---
+++ src/editorbuffer.h
@@ -0,0 +1,35 @@
+#ifndef EDITORBUFFER_H_INCLUDED
+#define EDITORBUFFER_H_INCLUDED
+
+#include <time.h>
+#include <stdbool.h>
+
+
+typedef struct {
+    int   bufferSize;
+    char *text;                 /* Where is the actual buffer? */
+    int   allocatedFreePrefixSize; /* How much space have we allocated *before* text? */
+    char *allocatedBlock;
+    int   allocatedIndex;
+    int   allocatedSize;
+} EditorBufferAllocationData;
+
+typedef struct editorBuffer {
+    char                      *name;
+    int                        fileNumber;
+    char                      *fileName;
+    time_t                     modificationTime;
+    bool                       textLoaded : 1;
+    bool                       modified : 1;
+    bool                       modifiedSinceLastQuasiSave : 1;
+    size_t                     size;
+    struct editorMarker       *markers;
+    EditorBufferAllocationData allocation;
+} EditorBuffer;
+
+typedef struct editorBufferList {
+    EditorBuffer            *buffer;
+    struct editorBufferList *next;
+} EditorBufferList;
+
+#endif
