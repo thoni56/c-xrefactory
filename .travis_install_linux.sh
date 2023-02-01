d9b44ac9687eefe15b730ea86f842a7c3664da89
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-01 16:03:06 +0100

    [build] Change how cgreen is installed

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,5 +1,6 @@
 wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-amd64.deb
-sudo apt install ./cgreen-1.6.1-amd64.deb
+sudo dpkg -i ./cgreen-1.6.1-amd64.deb
+cgreen-runner --version
 sudo apt install openjdk-8-jre
 lcov --version
 gcov --version
