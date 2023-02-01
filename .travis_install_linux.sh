20c13cd8b926d4b542f0af145919e6e34f12d0a4
Thomas Nilefalk <thomas@nilefalk.se>
2023-02-01 15:55:54 +0100

    [build] Correct link to cgreen 1.6.1 deb

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,5 +1,5 @@
-wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1_amd64.deb
-sudo apt install ./cgreen-1.6.1_amd64.deb
+wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-amd64.deb
+sudo apt install ./cgreen-1.6.1-amd64.deb
 sudo apt install openjdk-8-jre
 lcov --version
 gcov --version
