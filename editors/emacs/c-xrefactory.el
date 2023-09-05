207c4d272dec906650f0fafbab551ba14dde6a31
Thomas Nilefalk <thomas@nilefalk.se>
2023-09-05 21:40:11 +0200

    [emacs] Cosmetic changes in option descriptions

--- editors/emacs/c-xrefactory.el
+++ editors/emacs/c-xrefactory.el
@@ -477,7 +477,7 @@ C-xrefactory functions.
 	:group 'c-xrefactory-general)
 
       (defcustom c-xref-files-encoding 'generic
-	"This variable specifies C-xrefactory multi language file encoding.  Available values are 'generic', 'ascii', 'european', 'euc', 'sjis', 'utf' and 'project'. If you use only 7-bit ascii charset, set this option to 'ascii. If you use 8-bit europeen encoding, set this value to 'european. If you use a kind of EUC encoding (multiple 8-bits Japanese, Korean, ...), set it to 'euc. If you use Japanese SJIS encoding, set it to 'sjis. If you use one of unicode encodings (utf-8 or utf-16) set it to 'utf'. Otherwise, use 'generic settings, which should work fine at least for completions and browsing. The value 'project allows to specify encoding for each project separately. It is highly recommended to set this option to 'project and to specify encodings for each of your projects within project options."
+	"This variable specifies C-xrefactory multi language file encoding.  Available values are 'generic', 'ascii', 'european', 'euc', 'sjis', 'utf' and 'project'. If you use only 7-bit ascii charset, set this option to 'ascii. If you use 8-bit europeen encoding, set this value to 'european. If you use a kind of EUC encoding (multiple 8-bits Japanese, Korean, ...), set it to 'euc. If you use Japanese SJIS encoding, set it to 'sjis. If you use one of unicode encodings (utf-8 or utf-16) set it to 'utf'. Otherwise, use 'generic settings, which should work fine at least for completions and browsing. The value 'project' allows to specify encoding for each project separately. It is highly recommended to set this option to 'project' and to specify encodings for each of your projects within project options."
 	:type '(symbol)
 	:group 'c-xrefactory-general)
 
