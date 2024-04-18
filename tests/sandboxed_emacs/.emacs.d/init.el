420d198f83b05ec458d540f7260e7d9800f3f145
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2024-04-18 12:10:12 +0200

    [emacs] Minor tweaks to the sandboxed execution of Emacs with c-xref

--- tests/sandboxed_emacs/.emacs.d/init.el
+++ tests/sandboxed_emacs/.emacs.d/init.el
@@ -11,20 +11,3 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/c-xrefactory/editors/emacs")
 (load-file "~/.emacs.d/el-get/c-xrefactory/editors/emacs/c-xrefactory.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   (quote
-    ((eval c-set-offset
-       (quote arglist-close)
-       (quote 0))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
