f5a51e6ef71d6f980b7cde741b6924e61ce7de82
Thomas Nilefalk <thomas@nilefalk.se>
2024-04-24 16:47:01 +0200

    [build] Add certificate update to travis build

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -1,7 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y ca-certificates
+sudo update-ca-certificates
+lcov --version
+gcov --version
 wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-amd64.deb
 sudo dpkg -i ./cgreen-1.6.1-amd64.deb
 cgreen-runner --version
-sudo apt install openjdk-8-jre
-lcov --version
-gcov --version
 gem install coveralls-lcov
