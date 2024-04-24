59576a083350672d6df818e9dd69247ee76a0153
Thomas Nilefalk <thomas@nilefalk.se>
2024-04-24 17:03:12 +0200

    [build] Fix path to cgreen package download

--- .travis_install_linux.sh
+++ .travis_install_linux.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y ca-certificates
 sudo update-ca-certificates
 lcov --version
 gcov --version
-wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-amd64.deb
-sudo dpkg -i ./cgreen-1.6.1-amd64.deb
+wget https://github.com/cgreen-devs/cgreen/releases/download/1.6.1/cgreen-1.6.1-x86_64-linux-gnu.deb
+sudo dpkg -i ./cgreen-1.6.1-x86_64-linux-gnu.deb
 cgreen-runner --version
 gem install coveralls-lcov
