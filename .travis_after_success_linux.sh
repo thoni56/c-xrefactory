1f559507a95ac36eb8b394238d2f3b65c32953c3
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2024-04-30 11:58:28 +0200

    [build] Exclude all *.mock for 'coveralls'

--- .travis_after_success_linux.sh
+++ .travis_after_success_linux.sh
@@ -2,6 +2,6 @@
 lcov -q -d src -c -o coverage.info
 lcov -q --remove coverage.info '*.mock' '*.tab.c' '/usr/*' -o coverage.info
 #coveralls-lcov coverage.info
-coveralls -r src -b src
+coveralls -r src -b src -E '*.mock'
 # Move "stable" tag if on main
 ./.travis_merge_to_stable_maybe.sh
