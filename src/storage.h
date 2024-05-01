c8682cc46c0b3523315e489876b1e0eb12515097
Thomas Nilefalk <thomas@nilefalk.se>
2024-05-01 20:47:49 +0200

    [tidy] Remove unused storage enums

--- src/storage.h
+++ src/storage.h
@@ -1,17 +1,8 @@
 #ifndef STORAGE_H_INCLUDED
 #define STORAGE_H_INCLUDED
 
-/* Because of the macro magic we can't comment near the actual values
-   so here are some descriptions of some of the Usage values
-   (duplication so remember to change in here too):
-
-   StorageConstant       - enumerator definition
-   // some "artificial" Java storages
-   StorageMethod         - storage for class methods
-
-*/
-
 #include "enums.h"
+
 #define ALL_STORAGE_ENUMS(ENUM)             \
     ENUM(StorageDefault)                    \
     ENUM(StorageError)                      \
@@ -21,9 +12,7 @@
     ENUM(StorageStatic)                     \
     ENUM(StorageThreadLocal)                \
     ENUM(StorageTypedef)                    \
-    ENUM(StorageMutable)                    \
     ENUM(StorageRegister)                   \
-    ENUM(StorageMethod)                     \
     ENUM(MAX_STORAGE_NAMES)
     /* If this becomes more than 32 increase STORAGES_LN !!!!!!!! */
 
