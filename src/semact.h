3c7c98450ccb534113169f6103ec95b269d3c1dd
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2024-06-03 11:51:52 +0200

    [tidy] Fix incompatible enum types in parameter lists

--- src/semact.h
+++ src/semact.h
@@ -22,20 +22,20 @@ extern Result findStructureMemberSymbol(Symbol **res, StructMemberFindInfo *ss, 
 extern Symbol *addNewSymbolDefinition(SymbolTable *table, char *fileName, Symbol *symbol, Storage storage,
                                       UsageKind usage);
 extern Symbol *addNewDeclaration(SymbolTable *table, Symbol *baseType, Symbol *declaration, IdList *idList,
-                                 unsigned storage);
+                                 Storage storage);
 extern int styyerror(char *s);
 extern int styyErrorRecovery(void);
 extern void setToNull(void *p);
 extern Symbol *typeSpecifier1(unsigned t);
-extern void declTypeSpecifier1(Symbol *d, unsigned t);
+extern void declTypeSpecifier1(Symbol *d, Type type);
 extern Symbol *typeSpecifier2(TypeModifier *t);
 extern void declTypeSpecifier2(Symbol *d, TypeModifier *t);
 extern TypeModifier *addComposedTypeToSymbol(Symbol *symbol, Type type);
-extern TypeModifier *appendComposedType(TypeModifier **d, unsigned t);
+extern TypeModifier *appendComposedType(TypeModifier **d, Type type);
 extern void completeDeclarator(Symbol *t, Symbol *d);
 extern void addFunctionParameterToSymTable(SymbolTable *tab, Symbol *function, Symbol *p, int i);
 extern SymbolList *createDefinitionList(Symbol *symbol);
-extern Symbol *createSimpleDefinition(unsigned storage, unsigned t, Id *id);
+extern Symbol *createSimpleDefinition(Storage storage, Type type, Id *id);
 extern int findStructureMember(Symbol *symbol, char *memberName, Symbol	**foundMemberSymbol);
 extern Reference *findStuctureMemberFromSymbol(Symbol *str, Id *member, Symbol **res);
 extern Reference *findStructureFieldFromType(TypeModifier *structure, Id *field, Symbol **resultingSymbol);
