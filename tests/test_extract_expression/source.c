af83b4909889139ff451f7f3e8014eed47bbf6a0
Thomas Nilefalk <thomas@nilefalk.se>
2024-06-13 19:38:39 +0200

    [tests] Replace test_extract_expression with test_extract_variable

--- tests/test_extract_expression/source.c
+++
@@ -1,6 +0,0 @@
-static int a, b;
-
-void func(void) {
-    if (a == b)
-        return;
-}
---
+++ tests/test_extract_variable/source.c
@@ -0,0 +1,9 @@
+void f(void) {
+    int x, y, z;
+    int a = x+y+z;
+}
+
+void g(void) {
+    int x, y, z;
+    int a = x+y+z;
+}
