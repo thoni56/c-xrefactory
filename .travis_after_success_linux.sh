4baecc9b25b562d60abf3ccc2748e2d6ee40eb4e
Thomas Nilefalk <thomas@nilefalk.se>
2024-06-22 12:29:09 +0200

    [ci] Generate .gcov's with total coverage and let coveralls app do its thing

--- .travis_after_success_linux.sh
+++ .travis_after_success_linux.sh
@@ -1,7 +1,5 @@
-# Coverage for tests to Coveralls, but only for linux
-lcov -q -d src -c -o coverage.info
-lcov -q --remove coverage.info '*.mock' '*.tab.c' '/usr/*' -o coverage.info
-# coveralls -r src -b src -E '^.*\.mock$' -E '^.*\.tab\.c$'
+# The new coveralls app will automatically collect .gcov files
 coveralls report
+
 # Move "stable" tag if on main
 ./.travis_merge_to_stable_maybe.sh
