a5fec6ead28246a1bf8ed033ddc602bbc5c6002e
Thomas Nilefalk <thomas@nilefalk.se>
2024-11-28 09:16:09 +0100

    [unittest] Add one for allocate/free Completion

--- src/completion_tests.c
+++ src/completion_tests.c
@@ -14,6 +14,14 @@ Describe(Completion);
 BeforeEach(Completion) {}
 AfterEach(Completion) {}
 
-Ensure(Completion, can_compile_an_empty_test) {
-    olcxFreeCompletions(NULL);
+protected Completion *newCompletion(char *name, char *fullName, int lineCount, ReferenceCategory category,
+                                    Type csymType, struct reference ref, struct referenceItem sym);
+
+protected void olcxFreeCompletion(Completion *completion);
+
+Ensure(Completion, can_allocate_and_free_completions) {
+    Reference ref;
+    ReferenceItem item;
+    Completion *c = newCompletion("", "", 0, CategoryLocal, TypeInt, ref, item);
+    olcxFreeCompletion(c);
 }
