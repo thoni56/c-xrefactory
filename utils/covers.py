fcbe5aba877f8fcf5acf3581d8154cb63c837268
Thomas Nilefalk <thomas@nilefalk.se>
2024-12-02 23:14:09 +0100

    [util] Fix covers.py to work with new .cov subdir names

--- utils/covers.py
+++ utils/covers.py
@@ -12,20 +12,20 @@ parser.add_argument("line", help="the line that should be covered")
 
 args = parser.parse_args()
 
-dirs = map(os.path.dirname, glob.glob("../tests/*/.coverage"))
+dirs = map(os.path.dirname, glob.glob("../tests/*/.cov"))
 basename = args.file.rsplit( ".", 1 )[ 0 ]
-linenumber = ":{0:>5}:".format(int(args.line))
+linenumber_match = ":{0:>5}:".format(int(args.line))
 
 shutil.copy(os.path.join(".objects", basename+".gcda"), "/tmp")
 for d in dirs:
     try:
-        f = os.path.join(d, ".coverage", basename+".gcda")
+        f = os.path.join(d, ".cov", basename+".gcda")
         shutil.copy(f, os.path.join(os.getcwd(), ".objects"))
     except FileNotFoundError:
         continue
 
     result = subprocess.run(["gcov", ".objects/"+basename+".o"], capture_output=True, text=True)
-    result = subprocess.run(["grep", linenumber, basename+".c.gcov"], capture_output=True, text=True)
+    result = subprocess.run(["grep", linenumber_match, basename+".c.gcov"], capture_output=True, text=True)
     line = result.stdout
     if line != None:
         parts = line.split(':')
