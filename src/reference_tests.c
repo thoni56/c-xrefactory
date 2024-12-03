701517b89d90519bb613dc38e13433fa23870d29
Thomas Nilefalk <thomas@nilefalk.se>
2024-12-03 19:48:57 +0100

    [unittests] Add empty unittests for reference module

---
+++ src/reference_tests.c
@@ -0,0 +1,18 @@
+#include <cgreen/cgreen.h>
+#include <cgreen/unit.h>
+
+#include "reference.h"
+
+#include "options.mock"
+#include "misc.mock"
+#include "commons.mock"
+#include "filetable.mock"
+
+
+Describe(Reference);
+BeforeEach(Reference) {}
+AfterEach(Reference) {}
+
+
+Ensure(Reference, can_run_an_empty_test) {
+}
