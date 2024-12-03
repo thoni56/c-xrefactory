cd318295a496b5705c316f922f1c374fba0dad7e
Thomas Nilefalk <thomas@nilefalk.se>
2024-12-03 22:41:27 +0100

    [unittests] Added initial test for LIST_CONS()

--- src/list_tests.c
+++ src/list_tests.c
@@ -1,7 +1,10 @@
 #include <cgreen/cgreen.h>
+#include <cgreen/constraint_syntax_helpers.h>
 #include <cgreen/unit.h>
 
 #include "list.h"
+
+#include "reference.mock"
 
 
 Describe(List);
@@ -9,5 +12,14 @@ BeforeEach(List) {}
 AfterEach(List) {}
 
 
-Ensure(List, can_run_an_empty_test) {
+Ensure(List, cons_an_element_to_a_list_will_update_the_list_to_point_to_the_element) {
+    Reference *element = newReference((Position){0,1,2}, UsageDefined, NULL);
+    Reference *list = newReference((Position){0,1,2}, UsageDefined, NULL);
+    Reference *initial_list = list;
+
+    LIST_CONS(element, list);
+
+    assert_that(list, is_equal_to(element));
+    assert_that(list->next, is_equal_to(initial_list));
+    assert_that(element->next, is_equal_to(initial_list));
 }
