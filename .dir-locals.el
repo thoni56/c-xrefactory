e1b7e63459b3393c60ee98dd511f45e06df96c4e
Thomas Nilefalk <thomas@nilefalk.se>
2024-12-25 20:34:58 +0100

    [lsp] Fix lsp-mode configuration and example for 'c-mode'

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,7 +6,7 @@
             (indent-tabs-mode . nil)
             (c-file-style . "k&r")
             (c-basic-offset . 4)
-            (eval . (c-set-offset 'arglist-close '0))
+            ;;(eval . (c-set-offset 'arglist-close '0))
             (mode . cov)
             )
          )
