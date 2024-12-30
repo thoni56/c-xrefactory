47fd05263baa1bd46a294f10d29edafc337e1dad
Thomas Nilefalk <thomas@nilefalk.se>
2024-12-30 08:40:27 +0100

    [unittests] Add unittest for editorbuffer

--- src/editorbuffer_tests.c
+++ src/editorbuffer_tests.c
@@ -23,3 +23,25 @@ AfterEach(EditorBuffer) {}
 Ensure(EditorBuffer, can_free_empty_list_of_editor_buffers) {
     freeEditorBuffers(NULL);
 }
+
+Ensure(EditorBuffer, will_return_null_for_non_existing_file) {
+    always_expect(editorBufferIsMember, will_return(false));
+    expect(fileExists, will_return(false));
+
+    assert_that(findEditorBufferForFile("non-existing"), is_null);
+}
+
+Ensure(EditorBuffer, will_create_buffer_and_load_existing_file) {
+    always_expect(editorBufferIsMember, will_return(false));
+    expect(fileExists, will_return(true));
+    always_expect(isDirectory, will_return(false));
+    always_expect(fileSize, will_return(42));
+    always_expect(fileModificationTime, will_return(666));
+    always_expect(normalizeFileName_static, will_return("existing"));
+    expect(addEditorBuffer);
+    expect(addFileNameToFileTable);
+    expect(allocateNewEditorBufferTextSpace);
+    expect(loadFileIntoEditorBuffer);
+
+    assert_that(findEditorBufferForFile("existing"), is_not_null);
+}
