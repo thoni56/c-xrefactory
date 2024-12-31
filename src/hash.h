f5fe164eb3b6e98fe8e0599c4f4cef6b538c24d8
Thomas Nilefalk <thomas@nilefalk.se>
2024-12-31 12:23:45 +0100

    [tidy] Duplicate cxfile's hash macros with functions and compare

--- src/hash.h
+++ src/hash.h
@@ -1,12 +1,6 @@
 #ifndef HASH_H_INCLUDED
 #define HASH_H_INCLUDED
 
-#define SYMTAB_HASH_FUN_INC(oldval, charcode) {\
-    oldval+=charcode; oldval+=(oldval<<10); oldval^=(oldval>>6);\
-}
-#define SYMTAB_HASH_FUN_FINAL(oldval) {\
-    oldval+=(oldval<<3); oldval^=(oldval>>11); oldval+=(oldval<<15);\
-}
 
 extern unsigned hashFun(char *s);
 
