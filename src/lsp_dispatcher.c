627783ce9ed37012e8eb6fd42539208a58cab4ee
Thomas Nilefalk <thomas@nilefalk.se>
2025-01-12 12:03:04 +0100

    [tidy] Replace if-chain for LSP method with table and lookup

--- src/lsp_dispatcher.c
+++ src/lsp_dispatcher.c
@@ -8,34 +8,39 @@
 #include "lsp_handler.h"
 #include "log.h"
 
+typedef void (*Handler)(JSON *message);
+
+typedef struct dispatchEntry {
+    const char *method;
+    Handler handler;
+} DispatchEntry;
+
+DispatchEntry dispatch[] = {
+    {"initialize", handle_initialize},
+    {"shutdown", handle_shutdown},
+    {"exit", handle_exit},
+
+    {"textDocument/codeAction", handle_code_action},
+    {"textDocument/didOpen", handle_did_open}
+};
 
 LspReturnCode dispatch_lsp_message(JSON *message) {
 
     const char *method = get_json_string_item(message, "method");
-    if (strcmp(method, "initialize") == 0) {
-        log_trace("LSP: Dispatched 'initialize'");
-        handle_initialize(message);
-        return LSP_RETURN_OK;
-    } else if (strcmp(method, "shutdown") == 0) {
-        log_trace("LSP: Dispatched 'shutdown'");
-        handle_shutdown(message);
-        return LSP_RETURN_OK;
-    } else if (strcmp(method, "textDocument/codeAction") == 0) {
-        log_trace("LSP: Dispatched 'textDocument/codeAction'");
-        handle_code_action(message);
-        return LSP_RETURN_OK;
-    } else if (strcmp(method, "exit") == 0) {
-        log_trace("LSP: Dispatched 'initialize'");
-        handle_exit(message);
-        return LSP_RETURN_EXIT;
-    } else {
-        if (get_json_item(message, "id") != NULL) {
-            log_trace("LSP: Received unsupported request '%s'", method);
-            handle_method_not_found(message);
-        } else
-            log_trace("LSP: Received unsupported notification '%s'", method);
-        return LSP_RETURN_ERROR_METHOD_NOT_FOUND;
+    for (int i=0; i < sizeof(dispatch)/sizeof(dispatch[0]); i++) {
+        if (strcmp(method, dispatch[i].method) == 0) {
+            log_trace("LSP: Dispatched '%s'", method);
+            dispatch[i].handler(message);
+            if (strcmp(method, "exit") == 0)
+                return LSP_RETURN_EXIT;
+            else
+                return LSP_RETURN_OK;
+        }
     }
-
-    return LSP_RETURN_ERROR_JSON_PARSE_ERROR;
+    if (get_json_item(message, "id") != NULL) {
+        log_trace("LSP: Received unsupported request '%s'", method);
+        handle_method_not_found(message);
+    } else
+        log_trace("LSP: Received unsupported notification '%s'", method);
+    return LSP_RETURN_ERROR_METHOD_NOT_FOUND;
 }
