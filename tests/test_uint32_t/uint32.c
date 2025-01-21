2ac71aef8eedf50ec2dee4d98e341cf3a9a9bedd
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2025-01-21 14:59:23 +0100

    [tests] Rename all variables in a test to different names

--- tests/test_uint32_t/uint32.c
+++ tests/test_uint32_t/uint32.c
@@ -1,39 +1,39 @@
 #include <stdint.h>
 
-typedef int8_tt a;
-typedef int16_t a;
-typedef int32_t a;
-typedef int64_t a;
+typedef int8_t a;
+typedef int16_t b;
+typedef int32_t c;
+typedef int64_t d;
 
-typedef int_fast8_t a;
-typedef int_fast16_t a;
-typedef int_fast32_t a;
-typedef int_fast64_t a;
+typedef int_fast8_t e;
+typedef int_fast16_t f;
+typedef int_fast32_t g;
+typedef int_fast64_t h;
 
-typedef int_least8_t a;
-typedef int_least16_t a;
-typedef int_least32_t a;
-typedef int_least64_t a;
+typedef int_least8_t i;
+typedef int_least16_t j;
+typedef int_least32_t k;
+typedef int_least64_t l;
 
-typedef intmax_t a;
+typedef intmax_t m;
 
-typedef intptr_t a;
+typedef intptr_t n;
 
-typedef uint8_t a;
-typedef uint16_t a;
-typedef uint32_t a;
-typedef uint64_t a;
+typedef uint8_t o;
+typedef uint16_t p;
+typedef uint32_t q;
+typedef uint64_t r;
 
-typedef uint_fast8_t a;
-typedef uint_fast16_t a;
-typedef uint_fast32_t a;
-typedef uint_fast64_t a;
+typedef uint_fast8_t s;
+typedef uint_fast16_t t;
+typedef uint_fast32_t u;
+typedef uint_fast64_t v;
 
-typedef uint_least8_t a;
-typedef uint_least16_t a;
-typedef uint_least32_t a;
-typedef uint_least64_t a;
+typedef uint_least8_t x;
+typedef uint_least16_t y;
+typedef uint_least32_t z;
+typedef uint_least64_t å;
 
-typedef uintmax_t a;
+typedef uintmax_t ä;
 
-typedef uintptr_t a;
+typedef uintptr_t ö;
