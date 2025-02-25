bfb39ce7177ae926b51ad596cb1ecf7e9f000bc1
Thomas Nilefalk <thomas@nilefalk.se>
2025-02-25 11:03:19 +0100

    [tests] Auto return type is not allowed, even in C23

--- tests/test_c23/source.c
+++ tests/test_c23/source.c
@@ -1,5 +1,11 @@
 _BitInt(32) x;   // 32-bit integer
 unsigned _BitInt(64) y;  // 64-bit unsigned integer
 
-auto foo() { return 42; }  // Expected: Recognize `foo` as a function symbol
-auto bar() { return 3.14; }  // Expected: Recognize `bar` as a function symbol
+auto auto_int = 42;  // Expected: Recognize 'auto_int' as an int variable
+auto auto_float = 3.14; // Expected: Recognize 'auto_float' as a float variable
+
+struct S { int f, g; };
+auto s = (struct S){ .f = 1, .g = 2 };  // C23: Deduce 's' as 'struct S'
+
+char *utf8 = u8"Hello Marián!";  // UTF-8 string
+int c = 'abcd';                  // Multi-character constant
