4826cbf56bfa35d0e31ae5aaffbe004142008ee4
Thomas Nilefalk <thomas@nilefalk.se>
2025-02-25 09:52:41 +0100

    [tests] Add 'auto' return type to C23 test

--- tests/test_c23/source.c
+++ tests/test_c23/source.c
@@ -1,2 +1,5 @@
 _BitInt(32) x;   // 32-bit integer
 unsigned _BitInt(64) y;  // 64-bit unsigned integer
+
+auto foo() { return 42; }  // Expected: Recognize `foo` as a function symbol
+auto bar() { return 3.14; }  // Expected: Recognize `bar` as a function symbol
