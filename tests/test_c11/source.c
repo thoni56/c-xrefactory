2437a5d60e9ed34fce0a3329493e479c340bd579
Thomas Nilefalk <thomas@nilefalk.se>
2025-03-04 17:30:56 +0100

    [c11]  Add some more C11 features to test

--- tests/test_c11/source.c
+++ tests/test_c11/source.c
@@ -4,6 +4,7 @@ int _Atomic y;       // Equivalent syntax
 _Thread_local int tls_var;
 static _Thread_local int static_tls;
 extern _Thread_local int extern_tls;
+static thread_local float gnu_tls; /* GNU extension */
 
 _Static_assert(sizeof(int) == 4, "Unexpected int size");
 
@@ -14,3 +15,5 @@ alignas(32) double a32;  // ✅ Alternative from <stdalign.h>
 
 int alignment = _Alignof(double);  // ✅ Typically 8 or 16 bytes
 long size = alignof(int);        // ✅ Alternative from <stdalign.h>
+
+_Noreturn void no_return(void) {}
