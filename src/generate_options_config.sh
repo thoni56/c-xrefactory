ed4754bd247cf3c78a3a21279f01d0314feb04d8
Thomas Nilefalk <thomas@nilefalk.se>
2025-11-25 17:02:58 +0100

    [build] Always check for git tag changes
    Make .last_git_tag a .PHONY target so that it always checks whether
    the git tag has changed. Previously, once the file existed, make
    would never update it, causing the version string to be stale after
    creating new tags.
    
    To prevent file watchers from triggering infinite loops, modified
    generate_options_config.sh to only update options_config.h if the
    content actually changed (using cmp). This means:
    - Every make run checks if git tag changed
    - .last_git_tag only updates if tag changed
    - options_config.h only updates if content changed
    - Watchers only trigger if .h file timestamp changed

--- src/generate_options_config.sh
+++ src/generate_options_config.sh
@@ -3,4 +3,9 @@
 GIT_HASH=$(git describe --abbrev=5 --always --tags --dirty)
 VERSION=$(git describe --abbrev=0 --tags)
 
-sed "s/@GIT_HASH@/$GIT_HASH/g; s/@VERSION@/$VERSION/g" options_config.h.in > options_config.h
+sed "s/@GIT_HASH@/$GIT_HASH/g; s/@VERSION@/$VERSION/g" options_config.h.in > options_config.h.tmp
+if [ ! -f options_config.h ] || ! cmp -s options_config.h.tmp options_config.h; then
+    mv options_config.h.tmp options_config.h
+else
+    rm options_config.h.tmp
+fi
