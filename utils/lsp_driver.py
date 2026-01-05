bf634e038ef0523eef00dfd33d4f2d3cf2f2609c
Thomas Nilefalk <thomas@nilefalk.se>
2026-01-05 15:45:26 +0100

    [lsp][tests] Use CURDIR in input as it need to match real paths when running
    This required the `lsp-driver` to map "CURDIR" in the input.json to current path, when
    converting it to LSP frames.

--- utils/lsp_driver.py
+++ utils/lsp_driver.py
@@ -5,6 +5,7 @@ import time
 import json
 import argparse
 import ijson
+import os
 
 def calculate_content_length(payload):
     """Calculate the Content-Length of the JSON payload."""
@@ -15,13 +16,26 @@ def package_lsp_frame(payload):
     content_length = calculate_content_length(payload)
     return f"Content-Length: {content_length}\r\n\r\n{payload}"
 
-def send_requests(file_name, delay):
+def substitute_curdir(obj, curdir):
+    """Recursively substitute CURDIR in all string values of a JSON object."""
+    if isinstance(obj, dict):
+        return {k: substitute_curdir(v, curdir) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [substitute_curdir(item, curdir) for item in obj]
+    elif isinstance(obj, str):
+        return obj.replace("CURDIR", curdir)
+    else:
+        return obj
+
+def send_requests(file_name, delay, curdir):
     """Read JSON requests from a file and send them as LSP frames."""
     try:
         with open(file_name, 'r') as file:
             # Parse JSON objects one at a time
             for obj in ijson.items(file, 'item'):
                 try:
+                    # Substitute CURDIR with actual path
+                    obj = substitute_curdir(obj, curdir)
                     # Serialize object to JSON string
                     json_payload = json.dumps(obj, indent=2)
                     lsp_frame = package_lsp_frame(json_payload)
@@ -39,10 +53,11 @@ def main():
     parser = argparse.ArgumentParser(description="Send JSON requests from a file as LSP frames.")
     parser.add_argument("file", help="File containing JSON requests (array of JSON objects).")
     parser.add_argument("-d", "--delay", type=float, default=1.0, help="Delay between sending requests (in seconds).")
+    parser.add_argument("--curdir", default=os.getcwd(), help="The value to replace CURDIR in input file (default: current working directory).")
 
     args = parser.parse_args()
 
-    send_requests(args.file, args.delay)
+    send_requests(args.file, args.delay, args.curdir)
 
 if __name__ == "__main__":
     main()
