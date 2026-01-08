f4a89c6719c1eccc3b7d4a05234dd26d2bfc80d8
Thomas Nilefalk <thomas@nilefalk.se>
2026-01-08 20:54:21 +0100

    [tests] Remove a temporary dir from repo

--- tests/c-xref-mini/enums.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENUMS_H_INCLUDED
-#define ENUMS_H_INCLUDED
-
-/* Some CPP magic to be able to print enums as strings */
-/* Usage example in e.g. storage.h/storage.c */
-#define GENERATE_ENUM_VALUE(ENUM) ENUM,
-#define GENERATE_ENUM_STRING(STRING) #STRING,
-
-#endif
